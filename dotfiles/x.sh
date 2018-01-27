7df2dadc6cd1aed6fa5ca1bfa7c963f66fd94b76
İ. Göktuğ Kayaalp <self@gkayaalp.com>
2018-01-27 05:22:27 +0300

    ; add comment.

--- dotfiles/x.sh
+++ dotfiles/x.sh
@@ -4,9 +4,4 @@
 # by ‘dbus-launch’ and it starts dbus-dependent programs before it
 # hands the control to the window manager.
 
-# Set up the desktop.
-# -geometry args: wxh+x+y
-#xclock -geometry 125x125-0-0&
-#xterm -geometry 120x1-0+0 -T xstatus -e $MEINE/../scripts/sys-status.sh&
-
 exec i3 #vtwm -f $MEINE/twmrc
