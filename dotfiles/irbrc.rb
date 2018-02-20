4812b5bb537b1acb379c60991bb234cc5998d20b
İ. Göktuğ Kayaalp <self@gkayaalp.com>
2018-02-20 22:29:52 +0300

    Add IRB configuration.

---
+++ dotfiles/irbrc.rb
@@ -0,0 +1,9 @@
+IRB.conf[:PROMPT][:MY_PROMPT] = { # name of prompt mode
+  :AUTO_INDENT => true,            # enables auto-indent mode
+  :PROMPT_I => "> ",               # normal prompt
+  :PROMPT_S => "",               # prompt for continuated strings
+  :PROMPT_C => "",               # prompt for continuated statement
+  :RETURN => "=>%s\n"        # format to return value
+}
+
+IRB.conf[:PROMPT_MODE] = :MY_PROMPT
