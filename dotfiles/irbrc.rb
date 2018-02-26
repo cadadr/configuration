dcab150f0e96419f45a395cfbc84985cf63861cc
İ. Göktuğ Kayaalp <self@gkayaalp.com>
2018-02-26 17:00:27 +0300

    dotfiles/irbrc.rb: Use default prompt.

--- dotfiles/irbrc.rb
+++ dotfiles/irbrc.rb
@@ -1,9 +1,9 @@
 IRB.conf[:PROMPT][:MY_PROMPT] = { # name of prompt mode
   :AUTO_INDENT => true,            # enables auto-indent mode
-  :PROMPT_I => "> ",               # normal prompt
+  :PROMPT_I => IRB.conf[:PROMPT][:DEFAULT][:PROMPT_I], # keep normal prompt
   :PROMPT_S => "",               # prompt for continuated strings
   :PROMPT_C => "",               # prompt for continuated statement
-  :RETURN => "=>%s\n"        # format to return value
+  :RETURN => "=> %s\n"        # format to return value
 }
 
 IRB.conf[:PROMPT_MODE] = :MY_PROMPT
