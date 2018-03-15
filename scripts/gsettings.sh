f3ce7c780b5abb77c48e3cfdc661e6541f164ec6
İ. Göktuğ Kayaalp <self@gkayaalp.com>
2018-03-15 18:16:14 +0300

    scripts/gsettings.sh: Set emacs key theme for GTK.

--- scripts/gsettings.sh
+++ scripts/gsettings.sh
@@ -6,6 +6,8 @@ gsettings set org.gnome.desktop.input-sources xkb-options "['caps:ctrl_modifier'
 # Touchpad clicks: 1 finger: primary, 2 fingers: secondary, 3 fingers:
 # middle click.
 gsettings set org.gnome.desktop.peripherals.touchpad click-method 'fingers'
+# Emacs key theme
+gsettings set org.gnome.desktop.interface gtk-key-theme "Emacs"
 
 # Tell the user that this script was run.
 touch ~/.cf-scripts-gsettings-sh-was-applied
