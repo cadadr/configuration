c261d5a7cfab68f6d16467800da667e2e649f2d5
İ. Göktuğ Kayaalp <self@gkayaalp.com>
2018-03-19 04:41:17 +0300

    scripts/gsettings.sh: Make alt-tab behave.
    The default Alt-Tab behaviour of Gnome 3 is outright stupidly
    idiotically hostile.  If I put a thing on another workspace, that's
    because I do not want it on my way at the moment.  If Alt-Tab will
    cycle through all of them, what's the use of the workspace?  And if
    Alt-` is there, and Super+Tab switches between apps, why not keep
    Alt+Tab as "switch between windows in the current workspace"?  Them
    guys should quit writing code on drugs.

--- scripts/gsettings.sh
+++ scripts/gsettings.sh
@@ -9,5 +9,9 @@ gsettings set org.gnome.desktop.peripherals.touchpad click-method 'fingers'
 # Emacs key theme
 gsettings set org.gnome.desktop.interface gtk-key-theme "Emacs"
 
+# Sane Alt-Tab: switch between windows (not apps) in the current workspace.
+gsettings set org.gnome.desktop.wm.keybindings switch-windows "Alt-Tab"
+gsettings set org.gnome.shell.window-switcher current-workspace-only "true"
+
 # Tell the user that this script was run.
 touch ~/.cf-scripts-gsettings-sh-was-applied
