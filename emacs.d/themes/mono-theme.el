a6fb5738013be8dfc17487440ba72193e5982905
İ. Göktuğ Kayaalp <self@gkayaalp.com>
2018-03-29 19:52:20 +0300

    emacs.d/themes/mono-theme.el: better names for colours, nice org level colour.

--- emacs.d/themes/mono-theme.el
+++ emacs.d/themes/mono-theme.el
@@ -33,81 +33,83 @@
 (deftheme mono
   "A monochrome Emacs colour theme.")
 
-(let ((black "black")
-      (white "white")
-      (blue "#0087af")
-      (grey "#dee")
-      (green "#004953")                 ; midnight green
-      (khaki "khaki")
-      (beige "beige"))
+(let ((a "black")
+      (b "white")
+      (c "#0087af")
+      (d "#dee")
+      (e "violetred4")
+      (f "khaki")
+      (g "beige")
+      (h "hotpink"))
   (custom-theme-set-faces
    'mono
    ;; === Frame ===
-   `(default ((t (:foreground ,black :background ,white))))
-   `(mode-line ((t (:foreground ,white :background ,black :box nil))))
-   `(mode-line-inactive ((t (:background ,white :box t))))
-   `(mode-line-highlight ((t (:foreground ,black :background "grey" :box nil))))
-   `(region ((t (:background "hotpink"))))
+   `(default ((t (:foreground ,a :background ,b))))
+   `(mode-line ((t (:foreground ,b :background ,a :box nil))))
+   `(mode-line-inactive ((t (:background ,b :box t))))
+   `(mode-line-highlight ((t (:foreground ,a
+                                          :background ,h
+                                          :box nil))))
+   `(region ((t (:background ,h))))
 
    ;; === General ===
    `(link
-     ((t (:foreground ,blue :background ,white :underline t))))
+     ((t (:foreground ,c :background ,b :underline t))))
    `(highlight
-     ((t (:background ,grey))))
+     ((t (:background ,d))))
 
    ;; === Syntax ===
-   `(font-lock-builtin-face ((t (:foreground ,black))))
-   `(font-lock-comment-face ((t (:foreground ,black :weight bold))))
-   `(font-lock-string-face ((t (:foreground ,black))))
-   `(font-lock-function-name-face ((t (:foreground ,black))))
-   `(font-lock-variable-name-face ((t (:foreground ,black))))
-   `(font-lock-keyword-face ((t (:foreground ,black :weight bold))))
-   `(font-lock-type-face ((t (:foreground ,black))))
-   `(font-lock-constant-face ((t (:foreground ,black :weight bold))))
+   `(font-lock-builtin-face ((t (:foreground ,a))))
+   `(font-lock-comment-face ((t (:foreground ,a :weight bold))))
+   `(font-lock-string-face ((t (:foreground ,a))))
+   `(font-lock-function-name-face ((t (:foreground ,a))))
+   `(font-lock-variable-name-face ((t (:foreground ,a))))
+   `(font-lock-keyword-face ((t (:foreground ,a :weight bold))))
+   `(font-lock-type-face ((t (:foreground ,a))))
+   `(font-lock-constant-face ((t (:foreground ,a :weight bold))))
 
    ;; === Dired ===
    `(dired-directory ((t (:weight bold))))
 
    ;; === Diff ===
-   `(diff-header  ((t (:foreground ,black :background ,white))))
+   `(diff-header  ((t (:foreground ,a :background ,b))))
    `(diff-file-header
-     ((t (:foreground ,black :background ,white :weight bold))))
-   `(diff-context  ((t (:foreground ,black :background ,white))))
-   `(diff-added  ((t (:foreground ,black :background ,white))))
-   `(diff-removed  ((t (:foreground ,black :background ,white))))
-   `(diff-index  ((t (:foreground ,black :background ,white :weight bold))))
+     ((t (:foreground ,a :background ,b :weight bold))))
+   `(diff-context  ((t (:foreground ,a :background ,b))))
+   `(diff-added  ((t (:foreground ,a :background ,b))))
+   `(diff-removed  ((t (:foreground ,a :background ,b))))
+   `(diff-index  ((t (:foreground ,a :background ,b :weight bold))))
    `(diff-indicator-added
-     ((t (:foreground ,black :background ,white :weight bold))))
+     ((t (:foreground ,a :background ,b :weight bold))))
    `(diff-indicator-removed
-     ((t (:foreground ,black :background ,white :weight bold))))
+     ((t (:foreground ,a :background ,b :weight bold))))
    ;; === Shell scripts ===
-   `(sh-heredoc ((t (:foreground ,black))))
+   `(sh-heredoc ((t (:foreground ,a))))
 
    ;; === Org ===
    ;; XXX Make some org stuff italic (footnotes, links, etc,
    ;; or monochrome in some other way.
-   `(org-level-1 ((t (:foreground ,green))))
-   `(org-level-2 ((t (:foreground ,green))))
-   `(org-level-3 ((t (:foreground ,green))))
-   `(org-level-4 ((t (:foreground ,green))))
-   `(org-level-5 ((t (:foreground ,green))))
-   `(org-level-6 ((t (:foreground ,green))))
-   `(org-level-7 ((t (:foreground ,green))))
-   `(org-level-8 ((t (:foreground ,green))))
-   `(org-document-title ((t (:foreground ,black :weight bold))))
-   `(org-code ((t (:foreground ,green))))
-   `(org-block ((t (:foreground ,black))))
-   `(org-block-begin-line ((t (:background ,khaki))))
-   `(org-block ((t (:background ,beige))))
+   `(org-level-1 ((t (:foreground ,e))))
+   `(org-level-2 ((t (:foreground ,e))))
+   `(org-level-3 ((t (:foreground ,e))))
+   `(org-level-4 ((t (:foreground ,e))))
+   `(org-level-5 ((t (:foreground ,e))))
+   `(org-level-6 ((t (:foreground ,e))))
+   `(org-level-7 ((t (:foreground ,e))))
+   `(org-level-8 ((t (:foreground ,e))))
+   `(org-block ((t (:foreground ,a :background ,g))))
+   `(org-block-begin-line ((t (:background ,f))))
+   `(org-code ((t (:foreground ,e))))
+   `(org-document-title ((t (:foreground ,a :weight bold))))
 
    ;; === RMAIL ===
    `(rmail-highlight
-     ((t (:foreground ,black :background ,white :weight bold :underline t))))
+     ((t (:foreground ,a :background ,b :weight bold :underline t))))
 
    ;; === Whitespace-mode ===
-   `(whitespace-tab ((t (:background ,khaki))))
-   `(whitespace-line ((t (:background ,khaki))))
-   `(whitespace-trailing ((t (:background ,khaki))))))
+   `(whitespace-tab ((t (:background ,f))))
+   `(whitespace-line ((t (:background ,f))))
+   `(whitespace-trailing ((t (:background ,f))))))
 
 (provide-theme 'mono)
 ;;; mono-theme.el ends here
