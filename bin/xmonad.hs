748a755e3fcacbbaa2c82e0bc25afd7104061864
İ. Göktuğ Kayaalp <self@gkayaalp.com>
2018-04-12 22:19:10 +0300

    bin/xmonad.hs: Custom launchers.

--- bin/xmonad.hs
+++ bin/xmonad.hs
@@ -1,7 +1,6 @@
 -- gk-xmonad.hs --- Göktuğ's XMonad Configuration.
 
-import XMonad (xmonad, modMask, mod4Mask, workspaces, spawn,
-               xK_d, xK_n, xK_q)
+import XMonad
 import XMonad.Config.Desktop (desktopConfig)
 import XMonad.Util.EZConfig (additionalKeys)
 
@@ -9,12 +8,15 @@ main = xmonad $ desktopConfig {
     modMask = myModMask,
     -- First two workspaces are the most frequent ones.
     workspaces = ["home", "other"] ++ (map show $ takeWhile (<=9) [3..])
-  } `additionalKeys` keys
+  } `additionalKeys` myKeys
 
 myModMask = mod4Mask
+modShiftMask = myModMask .|. shiftMask
 
-keys = [((myModMask, xK_q), (spawn "start-qutebrowser.sh")),
-        -- Use the sct program to set screen redness.
-        ((myModMask, xK_n), (spawn "sct 4000")),
-        ((myModMask, xK_d), (spawn "sct 12000"))]
+myKeys = [((myModMask, xK_q), (spawn "start-qutebrowser.sh")),
+          -- Use the sct program to set screen redness.
+          ((myModMask, xK_n), (spawn "sct 4000")),
+          ((myModMask, xK_d), (spawn "sct 12000")),
+          ((myModMask, xK_p), (spawn "dmenu-desktop.pl")),
+          ((modShiftMask, xK_p), (spawn "dmenu_run"))]
 
