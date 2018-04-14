b6cd0321842e64f490bb5e1fb3fc222150aa764b
İ. Göktuğ Kayaalp <self@gkayaalp.com>
2018-04-15 00:28:11 +0300

    deb/DEBIAN/deps.mk: Add pinentry.

--- deb/DEBIAN/deps.mk
+++ deb/DEBIAN/deps.mk
@@ -19,7 +19,7 @@ DEPS+=laptop-mode-tools hdparm
 ### Desktop & Xorg:
 #### Base:
 DEPS+=sane sane-utils dbus-x11 feh dunst suckless-tools i3lock	\
-	notify-osd libnotify-bin
+	notify-osd libnotify-bin pinentry-gtk2
 #### XMonad:
 DEPS+=libghc-xmonad-dev libghc-xmonad-contrib-dev libghc-xmonad-doc \
 	libghc-xmonad-contrib-doc
