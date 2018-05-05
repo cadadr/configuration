d31e4c55edf9da9d8fde5c33e99b6ab4d90e0092
İ. Göktuğ Kayaalp <self@gkayaalp.com>
2018-05-05 03:28:11 +0300

    Add bin/sync-mailing-lists.sh.

---
+++ bin/sync-mailing-lists.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+# sync.sh --- sync mailing list archives.
+
+### Config:
+set -e
+
+LISTS_DIR=$HOME/posta/lists
+PARALLEL=2
+
+### Common:
+
+say(){
+    echo $0 [$(date)]: $@
+}
+
+### GNU lists:
+
+GNU_FTP=ftp://lists.gnu.org
+GNU_LISTS="help-gnu-emacs bug-gnu-emacs emacs-devel"
+
+gnu_fetch_list(){
+    lftp -c "set ftp:list-options -a;
+	open $GNU_FTP/;
+	lcd $1;
+	cd $1;
+	mirror --use-cache --verbose --allow-chown --parallel=$PARALLEL"
+}
+
+gnu_fetch_all(){
+    for list in $GNU_LISTS; do
+        say Syncing GNU:$list
+        mkdir -p $list
+        gnu_fetch_list "$list"
+    done
+}
+
+### Run:
+
+cd $LISTS_DIR
+say In $PWD:
+
+gnu_fetch_all
+
+say Finished.
