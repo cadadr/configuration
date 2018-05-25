80bb9170517d7b6544147dbbeca425f3b518ff73
İ. Göktuğ Kayaalp <self@gkayaalp.com>
2018-05-25 17:22:42 +0300

    deb/DEBIAN/deps.mk: Add libimage-exiftool-perl.

--- deb/DEBIAN/deps.mk
+++ deb/DEBIAN/deps.mk
@@ -4,11 +4,12 @@ DEPS=locales
 ### Libraries:
 DEPS+=libclass-isa-perl libswitch-perl liblockfile-bin libpam-systemd
 ### Utils:
-DEPS+=pass telnet file reportbug lsof gawk vim tmux units	    \
-	debian-goodies sudo vrms xz-utils alsa-utils bzip2 less	    \
-	sqlite3 zip unzip python3-reportbug groff-base gnupg2 lshw  \
-	openssh-client host texinfo cups-bsd cups-client bind9-host \
-	sloccount pandoc qemu hugo dirmngr pm-utils moreutils
+DEPS+=pass telnet file reportbug lsof gawk vim tmux units		\
+	debian-goodies sudo vrms xz-utils alsa-utils bzip2 less		\
+	sqlite3 zip unzip python3-reportbug groff-base gnupg2 lshw	\
+	openssh-client host texinfo cups-bsd cups-client bind9-host	\
+	sloccount pandoc qemu hugo dirmngr pm-utils moreutils		\
+	libimage-exiftool-perl
 ### Services:
 DEPS+=nginx syncthing openssh-server avahi-daemon avahi-autoipd
 ### Devtools:
