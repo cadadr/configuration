f91eb435408be1ee3df90c0fb82e14fb29ac2499
İ. Göktuğ Kayaalp <self@gkayaalp.com>
2018-06-13 17:51:55 +0300

    Undo last commit.

---
+++ emacs.d/packages/debbugs/debbugs-pkg.el
@@ -0,0 +1,2 @@
+;; Generated package description from debbugs.el
+(define-package "debbugs" "0.15" "SOAP library to access debbugs servers" '((soap-client "3.1.1") (cl-lib "0.5")) :url "http://elpa.gnu.org/packages/debbugs.html" :keywords '("comm" "hypermedia"))
