ea27a33828539d42f37c4c59079c200faef391a2
İ. Göktuğ Kayaalp <self@gkayaalp.com>
2018-08-05 15:03:09 +0300

    bin/dwm-config.h: toggle off floating bit for emacs

--- bin/dwm-config.h
+++ bin/dwm-config.h
@@ -29,7 +29,7 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Emacs",    NULL,       NULL,       0,            1,           -1 },
+	{ "Emacs",    NULL,       NULL,       0,            0,           -1 },
 	{ "Firefox",  NULL,       NULL,       1 << 1,       0,           -1 },
 };
 
