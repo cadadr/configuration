0234b231a163fd62ea40818ffa74d2f8e5100a59
İ. Göktuğ Kayaalp <self@gkayaalp.com>
2018-08-20 04:29:29 +0300

    arch/install-configs.sh: make directories if missing

--- arch/install-configs.sh
+++ arch/install-configs.sh
@@ -2,5 +2,6 @@
 
 for file in $(find etc/ -type f -not -name \*.in); do
     printf "Installing $file -> /$file... "
+    mkdir -p $(dirname /$file)
     cp -Pv --preserve=mode --backup=numbered $file /$file || echo Failed\!
 done
