65df00c35bd8c4d99a1b1143e0edd4640f66747c
İ. Göktuğ Kayaalp <self@gkayaalp.com>
2018-08-22 01:47:37 +0300

    dotfiles/x.sh: fix background and status display

--- dotfiles/x.sh
+++ dotfiles/x.sh
@@ -12,10 +12,11 @@ logx Start sbindkeys
 
 logx Set up workspace
 xclock -geometry 125x125-0-0 &
-xterm +sb -geometry 120x2-0+0 -T xstatus -e i3status &
+xterm +sb -fs 9 -geometry 35x7-3-130 -T xstatus -e\
+      'watch --interval 60 --no-title --color status.sh' &
 
 # Set the background.
-feh --no-fehbg --bg-fill ~/Pictures/Wallpapers/415656-2048x1365.jpg
+feh --no-fehbg --bg-fill ~/pic/wlp/415656-2048x1365.jpg &
 
 logx Start the window manager, xinit scripts completed
 exec vtwm -f $MEINE/twmrc
