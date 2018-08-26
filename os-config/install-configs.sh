5780bcf9efac8322cf66454a28a3fc61872db89f
İ. Göktuğ Kayaalp <self@gkayaalp.com>
2018-08-27 00:09:22 +0300

    os-config/install-configs.sh: install symlinks too and cp -u

--- os-config/install-configs.sh
+++ os-config/install-configs.sh
@@ -1,7 +1,6 @@
 # install-configs.sh
 
-for file in $(find etc/ -type f -not -name \*.in); do
-    printf "Installing $file -> /$file... "
+for file in $(find etc/ -type f -o -type l -not -name \*.in); do
     mkdir -p $(dirname /$file)
-    cp -Pv --preserve=mode --backup=numbered $file /$file || echo Failed\!
+    cp -Pvu --preserve=mode --backup=numbered $file /$file || echo Failed\!
 done
