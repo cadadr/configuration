b60ee053399118fe5e78ebf461872f3cee53f09f
İ. Göktuğ Kayaalp <self@gkayaalp.com>
2018-09-25 14:33:05 +0300

    emacs.d/init.el: new rmail flow
    Switch to a new Rmail flow where mail goes from inbox to current, and
    from there it’s refiled to archive when done with and deleted from
    current.  This will help separate unread messages from read ones
    better.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -2499,8 +2499,8 @@ Otherwise start mail program in offline mode."
 (setf
  rmail-primary-inbox-list gk-mail-inboxes
  rmail-secondary-file-directory gk-mail-home
- rmail-secondary-file-regexp "spam\\|outbox"
- rmail-file-name (expand-file-name "archive" gk-mail-home)
+ rmail-secondary-file-regexp "spam\\|outbox\\|archive$"
+ rmail-file-name (expand-file-name "current" gk-mail-home)
  rmail-displayed-headers
  (rx (and bol (or "to" "date" "from" "cc" "subject" "message-id" "list-id")))
  rmail-mime-prefer-html nil)
