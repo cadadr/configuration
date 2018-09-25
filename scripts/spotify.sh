5944a784ed477547160f0a3d7cfc36607a379a91
İ. Göktuğ Kayaalp <self@gkayaalp.com>
2018-09-25 14:34:02 +0300

    Update the spotify client installer script and add its dependencies

--- scripts/spotify.sh
+++ scripts/spotify.sh
@@ -1,14 +1,18 @@
 #!/bin/sh
 # spotify.sh --- install the spotify app.
 
-# Spotify app is proprietary, so is not included in goktug.deb.  This
-# script is adapted from the instructions on
+# Spotify app is proprietary, so is not included in packages.list.
+# This script is adapted from the instructions on
 # <https://www.spotify.com/tr/download/linux/>.
 
 set -e
 
+# This might be out of date, if apt reports that the package is not
+# signed, updating this from the page linked above can help.
+spotify_key=931FF8E79F0876134EDDBDCCA87FF9DF48BF1C90
+
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 \
-                 --recv-keys 0DF731E45CE24F27EEEB1450EFDC8610341D9410
+                 --recv-keys $spotify_key
 
 echo deb http://repository.spotify.com stable non-free \
     | sudo tee /etc/apt/sources.list.d/nonfree-spotify.list
