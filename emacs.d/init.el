0972500de151bd1b2f78210c36f7d48cef405685
İ. Göktuğ Kayaalp <self@gkayaalp.com>
2018-11-28 22:11:25 +0300

    emacs.d/init.el: new function ‘gk-next-theme’

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -577,6 +577,24 @@ the result."
            ".../" fnod)))
     name))
 
+(defun gk-next-theme ()
+  "Switch to the next theme in ‘custom-known-themes’.
+If exhausted, disable themes.  If run again thereafter, wrap to
+the beginning of the list."
+  (interactive)
+  (let* ((ct (or (car custom-enabled-themes)
+                 (car custom-known-themes)))
+         (next (cadr (memq ct custom-known-themes))))
+    (when (memq next '(user changed))
+      (setq next nil))
+    (dolist (theme custom-enabled-themes)
+      (disable-theme theme))
+    (if next
+        (progn
+          (load-theme next t)
+          (message "Loaded theme ‘%S’" next))
+      (message "All themes disabled"))))
+
 
 
 ;;;; Recompilation:
