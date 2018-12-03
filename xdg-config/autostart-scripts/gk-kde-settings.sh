a74245fca7c7b7dd1e61d5b0491189965a021c5d
İ. Göktuğ Kayaalp <self@gkayaalp.com>
2018-12-03 15:17:40 +0300

    Clean up
    Remove configuration that pertains to the XFCE4 desktop and related
    tools.  Remove also VSCode stuff.

--- xdg-config/autostart-scripts/gk-kde-settings.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# gk-kde-settings.sh --- KDE settings hard to do from the config interface
-
-# see: xinput list-props 'ETPS/2 Elantech Touchpad' | grep 'Click Method'
-xinput --set-prop 'ETPS/2 Elantech Touchpad' 296 0 1
