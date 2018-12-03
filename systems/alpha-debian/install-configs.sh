06f5a600de544d66e7c2b3635ccd445481a997e8
İ. Göktuğ Kayaalp <self@gkayaalp.com>
2018-12-03 14:58:29 +0300

    systems/alpha-debian/install-configs.sh: update system after installing configs

--- systems/alpha-debian/install-configs.sh
+++ systems/alpha-debian/install-configs.sh
@@ -4,3 +4,7 @@ for file in $(find etc/ -type f -o -type l -not -name \*.in); do
     mkdir -p $(dirname /$file)
     cp -Pvu --preserve=mode --backup=numbered $file /$file || echo Failed\!
 done
+
+# Update system
+locale-gen
+update-initramfs -u
