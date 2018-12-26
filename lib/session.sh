d813ccf1ba8ce669aa5f2ca8c566ecf98b0ab7de
İ. Göktuğ Kayaalp <self@gkayaalp.com>
2018-12-26 14:07:28 +0300

    lib/session.sh: do not start dbus if it’s started already

--- lib/session.sh
+++ lib/session.sh
@@ -5,10 +5,14 @@ tmpfil=$(mktemp)
 ssh-agent>$tmpfil
 . $tmpfil
 
-# Bookkeeping to use notify-send from processess started outside the X
-# session.  Mainly cron.
-dbusfil=/tmp/${USER}-dbus
-touch $dbusfil
-chmod 600 $dbusfil
-env | grep DBUS_SESSION_BUS_ADDRESS > $dbusfil
-echo 'export DBUS_SESSION_BUS_ADDRESS' >> $dbusfil
+if pgrep dbus >/dev/null; then
+	:;
+else
+	# Bookkeeping to use notify-send from processess started outside the X
+	# session.  Mainly cron.
+	dbusfil=/tmp/${USER}-dbus
+	touch $dbusfil
+	chmod 600 $dbusfil
+	env | grep DBUS_SESSION_BUS_ADDRESS > $dbusfil
+	echo 'export DBUS_SESSION_BUS_ADDRESS' >> $dbusfil
+fi
