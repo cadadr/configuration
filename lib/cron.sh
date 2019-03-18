d6e6307b066bad783516fd59004ce328d5c69e18
İ. Göktuğ Kayaalp <self@gkayaalp.com>
2019-03-18 18:21:00 +0300

    lib/cron.sh: don’t fail cronjobs when dbus is not available

--- lib/cron.sh
+++ lib/cron.sh
@@ -1,8 +1,14 @@
 # cron.sh --- common setup for cron scripts
 
 # Find dbus
-dbus="$(egrep -z DBUS < /proc/$(pgrep -u $LOGNAME i3\$)/environ | tr -d '\0')"
-eval "export $dbus"
+i3pid="$(pgrep -u $LOGNAME i3\$)"
+
+if [ -n "$i3pid" ]; then
+    dbus="$(egrep -z DBUS < /proc/$i3pid/environ | tr -d '\0')"
+    eval "export $dbus"
+else
+    export GK_CRON_NO_DBUS=yes
+fi
 
 # Load profile environment
 . $HOME/cf/lib/env.sh
