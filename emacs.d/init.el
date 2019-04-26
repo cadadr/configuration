adc4bde5bd4a58210a529becb619176223ad3c63
İ. Göktuğ Kayaalp <self@gkayaalp.com>
2019-04-26 03:10:47 +0300

    emacs.d/init.el: enable misterioso theme

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -3938,7 +3938,7 @@ Ask otherwise."
 
 ;;;;; GUI:
 
-(defvar gk-gui-theme nil
+(defvar gk-gui-theme 'misterioso
   "The default theme's name to load at startup.")
 
 (defun gk-setup-frame-looks (&optional frame)
