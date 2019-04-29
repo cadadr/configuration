5c49918557757fd44d56d0f9d894d482ec361339
İ. Göktuğ Kayaalp <self@gkayaalp.com>
2019-04-30 02:18:08 +0300

    Switch to Mate desktop

--- lib/xbackground.sh
+++
@@ -1,7 +0,0 @@
-# xbackground.sh --- start background applications
-
-redshift-gtk &
-nm-applet &			# network manager applet
-pasystray &			# pulse audio sound applet
-dunst &				# notifications daemon
-pcmanfm -d &			# PCManFM, can auto-mount stuff
