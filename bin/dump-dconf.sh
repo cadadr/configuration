322221ac7c4610ea76fc1ad394cfdf63fa86a787
İ. Göktuğ Kayaalp <self@gkayaalp.com>
2019-05-07 04:55:12 +0300

    bin/dump-dconf.sh: filter out window-position and window-ratio

--- bin/dump-dconf.sh
+++ bin/dump-dconf.sh
@@ -2,5 +2,5 @@
 # dump-dconf.sh --- dump dconf database near the db itself
 
 /usr/bin/dconf dump / \
-    | grep -Ev '^(session-start|geometry)=' \
+    | grep -Ev '^(session-start|geometry|window-position|window-ratio)=' \
            > $HOME/.config/dconf/user.dump
