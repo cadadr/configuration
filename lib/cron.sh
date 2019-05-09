6dfb3b316197ff0356e36d70e4cd6158634767c4
İ. Göktuğ Kayaalp <self@gkayaalp.com>
2019-05-10 02:21:56 +0300

    lib/cron.sh: use nm-applet to find dbus info

--- lib/cron.sh
+++ lib/cron.sh
@@ -1,10 +1,10 @@
 # cron.sh --- common setup for cron scripts
 
 # Find dbus
-i3pid="$(pgrep -u $LOGNAME i3\$)"
+pid="$(pgrep -u $LOGNAME nm-applet\$)"
 
-if [ -n "$i3pid" ]; then
-    dbus="$(egrep -z DBUS < /proc/$i3pid/environ | tr -d '\0')"
+if [ -n "$pid" ]; then
+    dbus="$(egrep -z DBUS < /proc/$pid/environ | tr -d '\0')"
     eval "export $dbus"
 else
     export GK_CRON_NO_DBUS=yes
