97ca921488f28b55ba618715e0bd2f727b12b5b2
İ. Göktuğ Kayaalp <self@gkayaalp.com>
2019-05-25 23:44:38 +0300

    lib/xbackground.sh: start flameshot with delay
    The indicator does not show up otherwise.

--- lib/xbackground.sh
+++ lib/xbackground.sh
@@ -5,5 +5,5 @@ nm-applet &			# network manager applet
 pasystray &			# pulse audio sound applet
 dunst &				# notifications daemon
 pcmanfm -d &			# PCManFM, can auto-mount stuff
-flameshot &			# screenshots
+( sleep 5 && flameshot; ) &	# screenshots
 xfce4-clipman &			# clipboard manager
