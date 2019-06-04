1cd5764d4fa34c586dbd76513478a270724a8be8
İ. Göktuğ Kayaalp <self@gkayaalp.com>
2019-06-04 15:31:32 +0300

    bin/passmenu.sh: show notification after copying

--- bin/passmenu.sh
+++ bin/passmenu.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 # passmenu.sh -- dmenu script for pass(1).
 
+set +e
+
 prefix=${PASSWORD_STORE_DIR-~/.password-store}
 prompt="Select password store enty"
 password=$(pass list | tail -n+2 | cut -d\  -f 2 | dmenu -p "$prompt :" -i)
 
 if [ x$password = x ]; then exit 1; fi
 
-exec pass show -c "$password"
-
+pass show -c "$password"
+notify-send "“$password” copied" \
+  "Password copied to clipboard, will be cleared after\
+ ${PASSWORD_STORE_CLIP_TIME:-45} seconds"
