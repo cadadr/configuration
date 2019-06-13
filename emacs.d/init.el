2bd03f324d2d5c6a6f34ff752d1f1377934dc9a3
İ. Göktuğ Kayaalp <self@gkayaalp.com>
2019-06-13 23:53:13 +0300

    emacs.d/init.el: add dvorak layout for quail

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1762,15 +1762,31 @@ will receive the region if active, or the entire buffer."
 
 ;; Quail is the Emacs core library for input methods.
 
-(setf
- quail-keyboard-layout
- (concat
-  "                              "
-  " 1!2\"3£4$5%6^7&8*9(0)[{]}    "
-  "  ’@,<.>pPyYfFgGcCrRlL/?=+   "
-  "   aAoOeEuUiIdDhHtTnNsS-_#~   "
-  "  \\|;:qQjJkKxXbBmMwWvVzZ      "
-  "                                "))
+(add-to-list
+ 'quail-keyboard-layout-alist
+ `("dvorak"
+   .
+   ,(concat
+     "                              "
+     "  1!2@3#4$5%6^7&8*9(0)[{]}`~  "
+     "  '\",<.>pPyYfFgGcCrRlL/?=+    "
+     "  aAoOeEuUiIdDhHtTnNsS-_\\|    "
+     "  ;:qQjJkKxXbBmMwWvVzZ        "
+     "                              ")))
+
+(add-to-list
+ 'quail-keyboard-layout-alist
+ `("brit-q"
+   .
+   ,(concat
+     "                              "
+     " 1!2\"3£4$5%6^7&8*9(0)[{]}    "
+     "  ’@,<.>pPyYfFgGcCrRlL/?=+   "
+     "   aAoOeEuUiIdDhHtTnNsS-_#~   "
+     "  \\|;:qQjJkKxXbBmMwWvVzZ      "
+     "                                ")))
+
+(quail-set-keyboard-layout "brit-q")
 
 (defvar gk-input-methods
   '("unilat-gk"
