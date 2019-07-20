f2d84aea873f5893670a1b591f06d6f84cba47c5
İ. Göktuğ Kayaalp <self@gkayaalp.com>
2019-07-20 17:33:16 +0300

    bin/make-backup.sh: handle the chores around setting up the backup

--- bin/make-backup.sh
+++ bin/make-backup.sh
@@ -3,8 +3,12 @@
 
 set -e
 
+. $MYLIB/fns.sh
+
 OUTDIR=${OUTDIR:=/backups}
 IGKDIR=${IGKDIR:=$MYFS}
+
+WAIT_SECS=${WAIT_SECS:=1}
 
 ballname="backup.$(date --utc +'%F.h%H%M.%Z')"
 ballext="tar.xz"
@@ -14,10 +18,10 @@ files="$(find /igk -maxdepth 1 				\
 	      -not \( -name '.*' -o -name 'lost+found'	\
 	      -o -name 'backup.*' -o -name 'tmp' \) 	)"
 
-# Count the number of files to be backed up, includes directories too.
+say Count the number of files to be backed up, includes directories too...
 count="$(find /igk/ | wc -l)"
 
-# Total size of the input.
+say Calculate total size of the input...
 insiz="$(du -hs $files | cut -f 1)"
 
 tarcmd="tar --force-local --preserve-permissions 		\
@@ -26,16 +30,55 @@ tarcmd="tar --force-local --preserve-permissions 		\
 
 pvcmd="pv --timer --eta --progress --size $count --line-mode -"
 
+say Close programs that keep $IGKDIR busy:
+for xpid in $(lsof -Fp "$IGKDIR" | egrep ^p); do
+    pid=${xpid/p}               # remove p prefix that lsof adds
+
+    [ "$pid" = "$$" ] && continue
+
+    # Safeguard for when some of the pids were the child of another one
+    if [ -e /proc/$pid ]; then
+        say Kill $(ps -q "$pid" -o comm=) \($pid\) with SIGTERM...
+
+        kill -TERM $pid
+
+        # Wait for the process to terminate
+        # From https://stackoverflow.com/a/19396161
+        #
+        # XXX(2019-07-20): may be Linux specific, but
+        # https://www.freebsd.org/doc/en/articles/linux-users/procfs.html
+        # says that procfs can be used in at least FreeBSD too.
+        while [ -e /proc/$pid ]; do
+            sleep $WAIT_SECS
+            # Report if will wait more
+            [ -e /proc/$pid ] && say waiting...
+        done
+ 
+        say "done"
+    fi
+done
+
+say Remount $IGKDIR as read-only \(will use sudo\)...
+sudo mount -o remount,ro $IGKDIR
+mount | grep $IGKDIR
+
+say Remove oldest of backups...
+rm -i "$(ls -t $OUTDIR/*.xz | tac | head -1)"*
+
+echo
 echo "Will back up $count items, $insiz of data in total..."
 echo "Input directory: $IGKDIR"
 echo "Output file: $OUTDIR/$ball"
 echo
+echo Hit Enter to continue
+read
 
+say Produce full backup:
 $tarcmd | $pvcmd >/dev/null
 
 chmod 400 "$OUTDIR/$ball"
 
-echo "Write MD5SUM..."
+say "Write MD5SUM..."
 ( cd $OUTDIR && md5sum $ball > $ball.md5sum )
 
 echo
