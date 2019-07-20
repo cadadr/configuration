27988fdaab6af2d19dfd5ed0f125e4ea7054fb92
İ. Göktuğ Kayaalp <self@gkayaalp.com>
2019-07-20 17:38:25 +0300

    lib/dump-dconf.sh: Filter out some more stuff

--- lib/dump-dconf.sh
+++ lib/dump-dconf.sh
@@ -2,5 +2,5 @@
 # dump-dconf.sh --- dump dconf database near the db itself
 
 /usr/bin/dconf dump / \
-    | grep -Ev '^(session-start|geometry|window-position|window-ratio)=' \
+    | grep -Ev '^(session-start|geometry|window-position|window-ratio|command-history|check-timestamp)=' \
            > $HOME/cf/lib/dconf.dump
