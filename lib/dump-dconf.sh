fb5d556bf0ef8436514e71ba873cd3d109cdba54
İ. Göktuğ Kayaalp <self@gkayaalp.com>
2019-08-03 19:12:45 +0300

    lib/dump-dconf.sh: ignore last-panel and application-id

--- lib/dump-dconf.sh
+++ lib/dump-dconf.sh
@@ -2,5 +2,5 @@
 # dump-dconf.sh --- dump dconf database near the db itself
 
 /usr/bin/dconf dump / \
-    | grep -Ev '^(session-start|geometry|window-position|window-ratio|command-history|check-timestamp)=' \
+    | grep -Ev '^(session-start|geometry|window-position|window-ratio|command-history|check-timestamp|last-panel|application-id)=' \
            > $HOME/cf/lib/dconf.dump
