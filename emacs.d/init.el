353d0bb68f554eb68b058bbbd3e2caf38887cf45
İ. Göktuğ Kayaalp <self@gkayaalp.com>
2019-09-15 03:30:53 +0300

    emacs.d/init.el (gk-setup-frame-looks): variable-pitch use serif

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -4413,7 +4413,7 @@ new frame is created."
 
   (set-face-attribute 'variable-pitch nil
                       :height gk-font-variable-pitch-height
-                      :family (gk-font :sans))
+                      :family (gk-font :serif))
 
   (loop for attr in '(mode-line mode-line-inactive) do
         (set-face-attribute attr nil
