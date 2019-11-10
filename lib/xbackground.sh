a4a3d3b47334473469722ba73a6275c3358fc360
İ. Göktuğ Kayaalp <self@gkayaalp.com>
2019-11-11 01:08:19 +0300

    lib/xbackground.sh: start emacs daemon

--- lib/xbackground.sh
+++ lib/xbackground.sh
@@ -8,3 +8,5 @@ dunst &				# notifications daemon
 pcmanfm -d &			# PCManFM, can auto-mount stuff
 ( sleep 5 && flameshot; ) &	# screenshots
 xfce4-clipman &			# clipboard manager
+# Start Emacs daemon
+( emacs --daemon && notify-send "Emacs ready!" ) &
