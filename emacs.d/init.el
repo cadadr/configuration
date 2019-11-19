a54568b50f043f38087e99f6441ce9c04ce03b5e
İ. Göktuğ Kayaalp <self@gkayaalp.com>
2019-11-20 01:02:50 +0300

    emacs.d/init.el (gk-home): display agenda taller

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -382,7 +382,7 @@ When ARG is a positive number, repeat that many times."
     (if (string= "rsc" (cdr (gk-i3wm-get-current-workspace-id)))
         (progn
           (other-window 1)
-          (split-window-vertically)
+          (split-window-vertically (* 4 (/ (window-height) 5)))
           (other-window 1)
           (find-file (gk-org-dir-file "okuma.org"))
           (other-window 1)
