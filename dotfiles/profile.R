7f4bed94b6697280e8220ec7204ef20d2a81ddba
İ. Göktuğ Kayaalp <self@gkayaalp.com>
2020-01-25 14:54:28 +0300

    dotfiles/profile.R: load psych::describe into default env

--- dotfiles/profile.R
+++ dotfiles/profile.R
@@ -1,5 +1,8 @@
 # profile.R --- R environment.
 
+# Load some useful libraries / functions.
+describe <- psych::describe
+
 # Disable scientific notation.
 options(scipen=999)
 # options(scipen=0) #enable scientific notation
