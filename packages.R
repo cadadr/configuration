3dbaf52074b3f4feb6aab5e093f5715303623b9f
İ. Göktuğ Kayaalp <self@gkayaalp.com>
2020-01-25 17:06:50 +0300

    Install R packages from Debian repos (instead of CRAN)

--- packages.R
+++
@@ -1,8 +0,0 @@
-# R packages
-
-# Tidyverse is a community maintained stdlib for R, I believe.
-install.packages("tidyverse")
-
-# Psych has ‘describe()’ which is a lot more detailed kind of
-# ‘summary()’.
-install.packages("psych")
