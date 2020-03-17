a330e4b4af0985a66322140b77dfdd434984e59b
İ. Göktuğ Kayaalp <self@gkayaalp.com>
2020-03-17 21:10:56 +0300

    bin/dump-dconf.sh: filter out virtual-root

--- bin/dump-dconf.sh
+++ bin/dump-dconf.sh
@@ -2,5 +2,5 @@
 # dump-dconf.sh --- dump dconf database near the db itself
 
 /usr/bin/dconf dump / \
-    | grep -Ev '^(session-start|geometry|window-position|window-ratio|command-history|check-timestamp|last-panel|application-id)=' \
+    | grep -Ev '^(session-start|geometry|window-position|window-ratio|command-history|check-timestamp|last-panel|application-id|virtual-root)=' \
            > $HOME/cf/lib/dconf.dump
