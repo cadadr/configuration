f3114f73efe9e871c09acbf839606d54ca403e6e
İ. Göktuğ Kayaalp <self@gkayaalp.com>
2020-03-18 00:49:29 +0300

    Remove bin/dump-dconf.sh, update lib/dconf-dump.sh
    Duplication is evil.

--- bin/dump-dconf.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# dump-dconf.sh --- dump dconf database near the db itself
-
-/usr/bin/dconf dump / \
-    | grep -Ev '^(session-start|geometry|window-position|window-ratio|command-history|check-timestamp|last-panel|application-id|virtual-root)=' \
-           > $HOME/cf/lib/dconf.dump
