952edf11ea936003d41a252b910f930a1b3fb021
İ. Göktuğ Kayaalp <self@gkayaalp.com>
2020-03-17 20:49:48 +0300

    bin/ffprofile.sh: use full path to firefox

--- bin/ffprofile.sh
+++ bin/ffprofile.sh
@@ -2,5 +2,5 @@
 
 grep ^Name= ~/.mozilla/firefox/profiles.ini | cut -d= -f2 \
     | dmenu -p 'Select Firefox profile:' \
-    | xargs -r firefox -P
+    | xargs -r ~/Applications/firefox/firefox -P
 
