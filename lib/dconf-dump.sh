7ecda810e606de79d511949960d3e0b3f0e33a0b
İ. Göktuğ Kayaalp <self@gkayaalp.com>
2020-03-22 04:21:51 +0300

    lib/dconf-dump.sh: move property names out of the regexp
    More readable and easier to edit this way.

--- lib/dconf-dump.sh
+++ lib/dconf-dump.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
 # dump-dconf.sh --- dump dconf database near the db itself
 
+filter="session-start
+geometry
+window-position
+window-ratio
+command-history
+check-timestamp
+last-panel
+application-id
+virtual-root
+extension-cache-updated"
+
 /usr/bin/dconf dump / \
-    | grep -Ev '^(session-start|geometry|window-position|window-ratio|command-history|check-timestamp|last-panel|application-id|virtual-root|extension-cache-updated)=' \
+    | grep -Ev "^($(echo $filter | tr ' ' '|'))=" \
            > $HOME/cf/lib/dconf.dump
