61037cd306b4f3472b7d99e3aea0fd8a5e03fc33
İ. Göktuğ Kayaalp <self@gkayaalp.com>
2020-04-30 03:05:00 +0300

    bin/ffprofile.sh: use system firefox and rofi

--- bin/ffprofile.sh
+++ bin/ffprofile.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 grep ^Name= ~/.mozilla/firefox/profiles.ini | cut -d= -f2 \
-    | dmenu -p 'Select Firefox profile:' \
-    | xargs -r ~/Applications/firefox/firefox -P
+    | rofi -dmenu -p 'Select Firefox profile:' \
+    | xargs -r firefox -P
 
