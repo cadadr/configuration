5eb922e343df23c010973e96434337129f2dbfc1
İ. Göktuğ Kayaalp <self@gkayaalp.com>
2020-05-01 00:34:55 +0300

    Restore bin/passmenu.sh, use rofi

---
+++ bin/passmenu.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# passmenu.sh -- rofi script for pass(1).
+
+set +e
+
+prefix=${PASSWORD_STORE_DIR-~/.password-store}
+prompt="Select password store enty"
+password=$(pass list | tail -n+2 | cut -d\  -f 2 \
+	| rofi -dmenu -p "$prompt :" -i)
+
+if [ x$password = x ]; then exit 1; fi
+
+pass show -c "$password"
+notify-send "“$password” copied" \
+  "Password copied to clipboard, will be cleared after\
+ ${PASSWORD_STORE_CLIP_TIME:-45} seconds"
