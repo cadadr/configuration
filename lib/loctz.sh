f72ec7749d7c327c5180dcbd074c2d1f678d1df0
İ. Göktuğ Kayaalp <self@gkayaalp.com>
2020-06-03 13:24:43 +0300

    lib/loctz.sh: do not use tr locales if not available
    Thus no breakage if locale-gen was not run.

--- lib/loctz.sh
+++ lib/loctz.sh
@@ -3,8 +3,10 @@
 MM_CHARSET=UTF-8;		export MM_CHARSET
 LANG=en_GB.$MM_CHARSET;		export LANG
 LANGUAGE=$LANG;			export LANGUAGE
-LC_MONETARY=tr_TR.$MM_CHARSET;	export LC_MONETARY
-LC_TIME=tr_TR.$MM_CHARSET;	export LC_TIME
+if [ -d /usr/share/locale/tr/ ]; then
+    LC_MONETARY=tr_TR.$MM_CHARSET;	export LC_MONETARY
+    LC_TIME=tr_TR.$MM_CHARSET;		export LC_TIME
+fi
 
 # Timezone
 TZ=Europe/Istanbul;	export TZ
