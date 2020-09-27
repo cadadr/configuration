1b9a5aed502fb74f5329996f423ffcac6f7d4291
İ. Göktuğ Kayaalp <self@gkayaalp.com>
2020-09-27 18:11:28 +0300

    lib/aliases.sh: add pi and pi_temp

--- lib/aliases.sh
+++ lib/aliases.sh
@@ -254,6 +254,9 @@ alias slrn="NNTPSERVER='news.gmane.io' HOME='$HOME/posta/news' slrn -i \
 alias pdf2doc='soffice --infilter="writer_pdf_import" --convert-to doc'
 alias pdf2odt='soffice --infilter="writer_pdf_import" --convert-to odt'
 
+# raspberry pi
+alias pi="ssh pi@pi.local"
+alias pi_temp="ssh pi@pi.local vcgencmd measure_temp"
 
 ### OS specific aliases:
 
