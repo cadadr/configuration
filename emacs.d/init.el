2e34a9d7a38175655c2a605e4be25704d36e5bfd
İ. Göktuğ Kayaalp <self@gkayaalp.com>
2020-10-05 14:51:01 +0300

    dark themes

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -5511,7 +5511,7 @@ fragments"
 Returns :light if the preferred colour scheme is light,
 :no-preference if no preference is set, or :dark if the user
 prefers dark themes."
-  :light)
+  :dark)
 
 
 (defun gk-setup-frame-looks (&optional frame)
