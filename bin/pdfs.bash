fcbc7a99e2605de45ddf91dbfbf92358ecb48a6a
İ. Göktuğ Kayaalp <self@gkayaalp.com>
2020-12-13 17:29:05 +0300

    bin/pdfs.bash: parameterize pdf viewer

--- bin/pdfs.bash
+++ bin/pdfs.bash
@@ -6,12 +6,13 @@ set -euo pipefail
 IFS=$'\n\t'
 
 fil="${PDFS_SAVE_FILE:-$HOME/pdfs.dat}"
+cmd="${PDFS_OPEN_COMMAND:-okular}"
 
 case $@ in
     dump) lsof | grep \\.pdf | awk 'FS=" /" {print "/" $2}' \
                 | tail +2 | sort | uniq | tr '\n' '\0'      \
                                              > "$fil" ;
           echo Wrote $fil ;;
-    load) xargs -0 okular < "$fil" 2>/dev/null 1>/dev/null & ;;
+    load) xargs -0 $cmd < "$fil" 2>/dev/null 1>/dev/null & ;;
     *) echo "usage: $0 [dump|load]" ; exit 2 ;;
 esac
