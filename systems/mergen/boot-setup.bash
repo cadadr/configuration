fd8a2d73209d716507de9cd5888b5cf70b7dc2ab
İ. Göktuğ Kayaalp <self@gkayaalp.com>
2020-12-18 22:08:14 +0300

    systems/mergen/boot-setup.bash: annotate with a comment and some sources

--- systems/mergen/boot-setup.bash
+++ systems/mergen/boot-setup.bash
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 # boot-setup.bash --- enable hibernation and GRUB menu under ubuntu et al.
 
+# *Note* that this requires a good amount of swap space, preferably
+# 2*RAM.
+
 # bash strict mode
 set -euo pipefail
 IFS=$'\n\t'
@@ -13,18 +16,21 @@ fi
 
 
 # Enable boot menu with some timeout
+# Adapted from: https://livingthelinuxlifestyle.wordpress.com/2020/10/21/show-the-grub-boot-menu-in-linux-mint-at-startup/
 printf "GRUB_TIMEOUT_STYLE=menu\nGRUB_TIMEOUT=30\n" \
     | tee /etc/default/grub.d/99_showmenu.cfg > /dev/null
 
 
 # Enable resuming from hibernation
+# Adapted from: https://askubuntu.com/a/1241902
 swapuuid="$(grep ^UUID=.*swap /etc/fstab | cut -d ' ' -f 1)"
 echo "GRUB_CMDLINE_LINUX_DEFAULT=\"quiet splash resume=$swapuuid\"" \
     | tee /etc/default/grub.d/99_resume-from-hibernation.cfg \
           > /dev/null
 
 
-# Enable hibernation using GUI dialogs
+# Enable hibernation using GUI dialogs.
+# Adapted from: https://www.reddit.com/r/linuxmint/comments/93ta9u/
 cat \
     > /etc/polkit-1/localauthority/50-local.d/com.ubuntu.enable-hibernate.pkla \
     <<EOF
