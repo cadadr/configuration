9d5f3cbaddacee6f274e5ede56bc4fb023047f28
İ. Göktuğ Kayaalp <self@gkayaalp.com>
2021-02-05 23:49:11 +0300

    ; systems/guixtest: whitepace

--- systems/guixtest/system.scm
+++ systems/guixtest/system.scm
@@ -2,8 +2,8 @@
 ;; by the graphical installer.
 
 (use-modules (gnu)
-	     (gnu system nss)
-	     (srfi srfi-1))
+             (gnu system nss)
+             (srfi srfi-1))
 
 (use-service-modules desktop networking ssh xorg)
 
@@ -34,11 +34,11 @@
  (name-service-switch %mdns-host-lookup-nss)
  (services
   (cons* (service openssh-service-type)
-	 (service slim-service-type
-		  (slim-configuration
-		     (xorg-configuration (xorg-configuration (keyboard-layout keyboard-layout)))))
+         (service slim-service-type
+                  (slim-configuration
+                   (xorg-configuration (xorg-configuration (keyboard-layout keyboard-layout)))))
          (remove (lambda (s) (eq? (service-kind s) gdm-service-type))
-		 %desktop-services)))
+                 %desktop-services)))
  (bootloader
   (bootloader-configuration
    (bootloader grub-efi-bootloader)
@@ -47,14 +47,14 @@
  (swap-devices
   (list (uuid "14dee6a1-c28e-49cf-a274-6ad9fc8ff500")))
  (file-systems
-    (cons* (file-system
-             (mount-point "/boot/efi")
-             (device (uuid "82F8-18A6" 'fat32))
-             (type "vfat"))
-           (file-system
-             (mount-point "/")
-             (device
-               (uuid "e4058621-c98a-493f-8a30-a58d03f9db6f"
-                     'ext4))
-             (type "ext4"))
+  (cons* (file-system
+          (mount-point "/boot/efi")
+          (device (uuid "82F8-18A6" 'fat32))
+          (type "vfat"))
+         (file-system
+          (mount-point "/")
+          (device
+           (uuid "e4058621-c98a-493f-8a30-a58d03f9db6f"
+                 'ext4))
+          (type "ext4"))
          %base-file-systems)))
--- systems/guixtest/user.scm
+++ systems/guixtest/user.scm
@@ -1,7 +1,7 @@
 ;;; manifest.scm --- packages to be installed for my profile, some customised
 
 (use-modules (gnu)
-	     (guix download)
+             (guix download)
              (guix packages)
              (srfi srfi-1))
 
@@ -37,55 +37,55 @@
 
 ;; emacs will need libjansson, also check emacs-telega for telegram
 (packages->manifest
-  (append
-    ;; Build dependencies for Emacs.
-    (cons* jansson
-           (map cadr (package-inputs emacs-next)))
-    ;; Build dependencies for Emacs pdf-tools.
-    (map cadr (package-inputs emacs-pdf-tools))
- (list
-  ;; linguistics
-  praat
-  ;; desktop apps
-  ;; XXX okular
-  audacity gk-qutebrowser cheese gimp gparted inkscape
-  mpv stellarium libreoffice xpdf
-  ;; tui apps
-  lynx mutt tmux w3m
-  ;; xorg / desktop utilities
-  clipit rofi simplescreenrecorder xclip desktop-file-utils
-  xinput xrdb setxkbmap
-  ;; network utilities
-  (list isc-bind "utils") bridge-utils ndisc6
-  ;; encryption, privacy & security
-  gnupg password-store pwgen
-  ;; programming
-  gnu-make python python-pip gcc-toolchain ruby bundler cmake gdb
-  gfortran-toolchain julia python-setuptools perl m4
-  ;; development libraries
-  perl-image-exiftool perl-switch perl-universal-isa
-  ;; maths, data, statistics & plotting
-  gnuplot r r-car r-psych r-rio r-tidyverse sqlite
-  units graphviz
-  ;; version control (git is installed in guix-system.scm)
-  cvs mercurial rcs subversion
-  ;; documents & authoring
-  djvulibre pandoc pandoc-citeproc pdfarranger stapler
-  texlive hunspell
-  (specification->package "hunspell-dict-fr-toutesvariantes")
-  hunspell-dict-it-it
-  hunspell-dict-en hunspell-dict-en-us hunspell-dict-en-gb
-  hunspell-dict-en-ca hunspell-dict-en-au
-  hunspell-dict-de
-  ;; command-line utilities
-  entr inotify-tools jq mailutils mb2md moreutils pv
-  qrencode youtube-dl vim-full
-  ;; e-mail
-  mpop msmtp procmail
-  ;; virtualisation
-  qemu
-  ;; system utilities
-  smartmontools strace)))
+ (append
+  ;; Build dependencies for Emacs.
+  (cons* jansson
+         (map cadr (package-inputs emacs-next)))
+  ;; Build dependencies for Emacs pdf-tools.
+  (map cadr (package-inputs emacs-pdf-tools))
+  (list
+   ;; linguistics
+   praat
+   ;; desktop apps
+   ;; XXX okular
+   audacity gk-qutebrowser cheese gimp gparted inkscape
+   mpv stellarium libreoffice xpdf
+   ;; tui apps
+   lynx mutt tmux w3m
+   ;; xorg / desktop utilities
+   clipit rofi simplescreenrecorder xclip desktop-file-utils
+   xinput xrdb setxkbmap
+   ;; network utilities
+   (list isc-bind "utils") bridge-utils ndisc6
+   ;; encryption, privacy & security
+   gnupg password-store pwgen
+   ;; programming
+   gnu-make python python-pip gcc-toolchain ruby bundler cmake gdb
+   gfortran-toolchain julia python-setuptools perl m4
+   ;; development libraries
+   perl-image-exiftool perl-switch perl-universal-isa
+   ;; maths, data, statistics & plotting
+   gnuplot r r-car r-psych r-rio r-tidyverse sqlite
+   units graphviz
+   ;; version control (git is installed in guix-system.scm)
+   cvs mercurial rcs subversion
+   ;; documents & authoring
+   djvulibre pandoc pandoc-citeproc pdfarranger stapler
+   texlive hunspell
+   (specification->package "hunspell-dict-fr-toutesvariantes")
+   hunspell-dict-it-it
+   hunspell-dict-en hunspell-dict-en-us hunspell-dict-en-gb
+   hunspell-dict-en-ca hunspell-dict-en-au
+   hunspell-dict-de
+   ;; command-line utilities
+   entr inotify-tools jq mailutils mb2md moreutils pv
+   qrencode youtube-dl vim-full
+   ;; e-mail
+   mpop msmtp procmail
+   ;; virtualisation
+   qemu
+   ;; system utilities
+   smartmontools strace)))
 
 ;; The following names are of Debian / Ubuntu packages from old `mergen' config that were not available or necessary under the new GuixSD configuration.
 
