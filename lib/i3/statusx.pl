13cdbdbbe8528ffc2ce2be334c3718b027a3be0d
İ. Göktuğ Kayaalp <self@gkayaalp.com>
2021-02-14 12:05:26 +0300

    host-specific i3status config, extended through filter script

---
+++ lib/i3/statusx.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/env perl
+# statusx.pl --- extend i3status output
+
+# Adapted from: https://github.com/i3/i3status/blob/master/contrib/wrapper.pl
+
+use v5.24;
+use strict;
+use warnings;
+
+use JSON;
+
+# Skip first two lines
+print scalar <STDIN>;
+print scalar <STDIN>;
+
+while (my ($statusline) = (<STDIN> =~ /^,?(.*)/)) {
+    my @blocks = @{decode_json($statusline)};
+
+	my $hostname = `hostname -f`; chomp $hostname;
+    @blocks = ({
+        full_text => "$ENV{'USER'}@" . $hostname,
+        name => 'u@h'
+    }, @blocks);
+
+    print encode_json(\@blocks) . ",\n";
+}
