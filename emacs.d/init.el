4b1af4e7b3b0d0d22bc1fb11b1522a4c5e3cfada
İ. Göktuğ Kayaalp <self@gkayaalp.com>
2021-02-25 00:14:41 +0300

    emacs.d/init.el (gk-org-todo-delayed-done): new function

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -4604,6 +4604,22 @@ modified slightly before it’s used e.g. when posting to Reddit."
   (dolist (link org-stored-links)
     (insert (apply #'format "- [[%s][%s]]\n" link))))
 
+
+;; From: https://stackoverflow.com/a/28130043
+(defun gk-org-todo-delayed-done (&optional arg)
+  "Like org-todo-yesterday, but prompt the user for a date. The time
+of change will be 23:59 on that day"
+  (interactive "P")
+  (let* ((hour (nth 2 (decode-time
+                       (org-current-time))))
+         (daysback (- (date-to-day (current-time-string))
+                      (org-time-string-to-absolute (org-read-date))))
+         (org-extend-today-until (+ 1 (* 24 (- daysback 1)) hour))
+         (org-use-effective-time t)) ; use the adjusted timestamp for logging
+    (if (eq major-mode 'org-agenda-mode)
+        (org-agenda-todo arg)
+      (org-todo arg))))
+
 
 
 ;;;;; Variables:
