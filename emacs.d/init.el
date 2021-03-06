61bd2fb243ebdceb3f725b580a4dbe5c2ee2f460
İ. Göktuğ Kayaalp <self@gkayaalp.com>
2021-03-06 21:44:22 +0300

    emacs.d/init.el: don’t load org-phscroll
    Breaks redisplay or window deletion somehow, when the buffer that pops
    up is a narrowed org buffer.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -154,7 +154,6 @@
 (require 'org-inlinetask)
 (require 'org-mobile)
 (require 'org-num)
-(require 'org-phscroll)
 (require 'org-protocol)
 (require 'org-tempo)                    ; <s, <q &c
 (require 'org-variable-pitch)
