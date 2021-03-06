82991c54be21f27210017c44e89dcaa96a7f33c4
İ. Göktuğ Kayaalp <self@gkayaalp.com>
2021-03-06 21:45:34 +0300

    emacs.d/init.el: larger fonts because my eyes are crying

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1693,8 +1693,8 @@ Tries to preserve the order of window buffers and active window."
 
 ;; Set up so that there's 80-85 chars width for half-sized horizontal
 ;; windows.
-(defconst gk-font-default-height 90)
-(defconst gk-font-variable-pitch-height 100)
+(defconst gk-font-default-height 100)
+(defconst gk-font-variable-pitch-height 110)
 
 (defun gk-font (type)
   "Get default font for TYPE, a keyword.
