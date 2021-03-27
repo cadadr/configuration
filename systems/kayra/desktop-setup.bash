8d5f8cccbbdafb9f62b5d9247b057e81249502a2
İ. Göktuğ Kayaalp <self@gkayaalp.com>
2021-03-27 16:44:04 +0300

    systems/kayra/desktop-setup.bash: middle button scroll from mergen

--- systems/kayra/desktop-setup.bash
+++ systems/kayra/desktop-setup.bash
@@ -18,6 +18,10 @@ xset -b
 # Swap mouse back/forward buttons around.
 xinput set-button-map "USB Optical Mouse" 1 2 3 4 5 6 7 9 8
 
+# Hold middle button to scroll in all directions.
+xinput set-prop "USB Optical Mouse" \
+	'libinput Scroll Method Enabled' {0,0,1}
+
 # Use XToolkit in java applications
 export AWT_TOOLKIT=XToolkit
 
