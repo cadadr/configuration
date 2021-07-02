b207a477bd9d1e5791e6f5b5a4128f01beb23b9c
İ. Göktuğ Kayaalp <self@gkayaalp.com>
2021-07-02 09:44:35 +0300

    systems/midori/install-packages.sh (pkgs_Desktop): add i3lock

--- systems/midori/install-packages.sh
+++ systems/midori/install-packages.sh
@@ -10,7 +10,7 @@ pkgs_Desktop="xorg xorg-apps xorg-xinit xf86-video-ati xf86-video-amdgpu \
         mesa qtile i3-gaps kitty lm_sensors python-psutil python-setproctitle \
         xterm rofi rofimoji blueman network-manager-applet dex dunst \
         pcmanfm-gtk3 xclip xdotool xpra feh flameshot seahorse picom \
-        i3status python-i3ipc redshift xfce4-clipman-plugin"
+        i3status python-i3ipc redshift xfce4-clipman-plugin i3lock"
 
 # TODO: jack instead of alsa
 pkgs_Audio="alsa-utils pulseaudio pulseaudio-alsa pasystray pavucontrol \
