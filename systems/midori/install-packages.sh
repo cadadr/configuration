d21edf3096175f0a738d8243a72038a7c110a936
İ. Göktuğ Kayaalp <self@gkayaalp.com>
2021-07-21 20:06:33 +0300

    systems/midori/install-packages.sh: add xdg-user-dirs and xf86-video-fbdev

--- systems/midori/install-packages.sh
+++ systems/midori/install-packages.sh
@@ -11,7 +11,8 @@ pkgs_Desktop="xorg xorg-apps xorg-xinit xf86-video-ati xf86-video-amdgpu \
 	xterm rofi rofimoji blueman network-manager-applet dex dunst \
 	pcmanfm xclip xdotool xpra feh flameshot seahorse picom \
 	i3status python-i3ipc redshift xfce4-clipman-plugin i3lock \
-	libmtp gvfs mtpfs android-file-transfer"
+	libmtp gvfs mtpfs android-file-transfer xdg-user-dirs \
+	xf86-video-fbdev"
 
 # TODO: jack instead of alsa
 pkgs_Audio="alsa-utils pulseaudio pulseaudio-alsa pasystray pavucontrol \
