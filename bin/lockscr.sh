d07eb096fa8892209085c42d812f660f5a36d2d1
İ. Göktuğ Kayaalp <self@gkayaalp.com>
2021-08-06 02:05:08 +0300

    Remove bin/lockscr.sh

--- bin/lockscr.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# lockscr.sh --- lock screen in X session
-
-bg="${GK_XBGIMG-$HOME/.xbg.png}"
-
-# if image not found, display a gray screen
-i3lock -p default -f -e -i "$bg" -c 333333 && sleep 1 || exit 2
-
