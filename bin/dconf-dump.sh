b72e44e281f898dafc4c5fb2e51a92b7a44ba2a1
İ. Göktuğ Kayaalp <self@gkayaalp.com>
2021-08-11 20:33:34 +0300

    Put dconf dumper filters in external file
    Now that bin/dconf-dump.sh is a daemon-like script, modifications to
    filters would require restarting it, which is inconvenient.
    
    Here we move filters to lib/dconf.filter, and load that file every
    loop before calling `dconf dump`, to make things more convenient.

--- bin/dconf-dump.sh
+++ bin/dconf-dump.sh
@@ -11,78 +11,6 @@ fi
 
 echo $$ > $pidfile
 
-filter="session-start
-geometry
-window-position
-window-ratio
-command-history
-check-timestamp
-last-panel
-application-id
-virtual-root
-extension-cache-updated
-refresh-last-run
-history-search-for
-installed-apps
-document-directory
-selected-files
-current-folder
-window-size
-maximized
-last-viewed-location
-transportation-type
-window-maximized
-sidebar-width
-sidebar-size
-save-directory
-show-hidden-files
-size
-state
-window-state
-window-height
-window-width
-device-aliases
-numlock-state
-volume
-volume-sound-enabled
-num-workspaces
-location-mode
-last-node-selected
-last-window-state
-saved-view
-selected-color
-custom-colors
-last-active-tool
-last-right-rgba
-last-size
-last-left-rgba
-current-tab
-window-pane-position
-recent-emoji
-sort-col
-picture-uri
-ignore-ca-cert
-ignore-phase2-ca-cert
-last-char
-favorite-apps-list
-looking-glass-history
-saved-pathbar-path
-night-light-last-coordinates
-last-refresh-time
-last-active-shape
-last-font-name
-sort-order
-sort-column
-col-[0-9]+-width
-api-caches
-focused-document-view[0-9]+
-opened-files-view[0-9]+
-last-folder-uri
-tab-info-list
-tabs
-position
-my-computer-expanded"
-
 # This pops up sometimes for some reason
 if [ "$DESKTOP_SESSION" = "cinnamon" ]; then
     dconf reset -f /org/mate/
@@ -92,6 +20,7 @@ say "started dconf dumper..."
 
 while true; do
     sleep 360s
+    . $MYLIB/dconf.filter
     say "dumping dconf database for $MYSYSTEM..."
     /usr/bin/dconf dump / \
 	| grep -Ev "^($(echo $filter | tr ' ' '|'))=" \
