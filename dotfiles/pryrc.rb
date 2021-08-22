afda5d9a9307ff380f5f793b31c3dcada2e00b43
İ. Göktuğ Kayaalp <self@gkayaalp.com>
2021-08-22 17:28:55 +0300

    Add pry repl for Ruby

---
+++ dotfiles/pryrc.rb
@@ -0,0 +1,7 @@
+# pryrc.rb --- pry ruby repl config
+
+# Aliases
+Pry.commands.alias_command :c, "continue"
+Pry.commands.alias_command :q, "exit"
+Pry.commands.alias_command :s, "step"
+Pry.commands.alias_command :n, "next"
