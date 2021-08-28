6737f82876452f2de3a20b88ad556cceb4a8829b
İ. Göktuğ Kayaalp <self@gkayaalp.com>
2021-08-28 14:00:46 +0300

    lib/setup.sh: call pip with --no-warn-script-location

--- lib/setup.sh
+++ lib/setup.sh
@@ -4,7 +4,9 @@ crontab cron/$(hostname).crontab
 
 update-desktop-database ~/.local/share/applications/
 
-pip3 install -r requirements.txt
+# scripts won't be in $PATH before reloading profile, so stop bugging me
+# 'bout it ffs.
+pip3 install --no-warn-script-location -r requirements.txt
 gem install --user-install bundler
 
 . lib/profile/paths.sh
