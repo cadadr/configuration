7e4dfea1327556f23ab562bdddccac1bdd47bb2c
İ. Göktuğ Kayaalp <self@gkayaalp.com>
2021-09-26 17:21:56 +0300

    lib/cron.sh: update how dbus is found

--- lib/cron.sh
+++ lib/cron.sh
@@ -1,6 +1,9 @@
 # cron.sh --- common setup for cron scripts
 
-env=/proc/$(pgrep -f "^$SESSION_MANAGER" -u "$USER")/environ
+env=/proc/$(pgrep -f "cinnamon-session --session cinnamon" -u "$USER" \
+                | cut -d ' ' -f 1)/environ
 
-export $(cat $env | egrep -z '^DBUS_SESSION_BUS_ADDRESS=')
+if [ -e "$env" ]; then
+    export $(cat $env | egrep -z '^DBUS_SESSION_BUS_ADDRESS=' | tr -d '\0')
+fi
 
