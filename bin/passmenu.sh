728ad03e6e8b138a29f122cdf4f340b1ddb6ea21
İ. Göktuğ Kayaalp <self@gkayaalp.com>
2021-10-01 19:09:02 +0300

    bin/passmenu.sh: touch up notifications

--- bin/passmenu.sh
+++ bin/passmenu.sh
@@ -7,19 +7,20 @@ prefix=${PASSWORD_STORE_DIR-~/.password-store}
 prompt="Select password store enty"
 password="$(pass git ls-files | grep -v ^\\. | sed s/.gpg\$// \
                  | rofi -dmenu -p "$prompt" -i)"
+notify="notify-send -i dialog-password"
 
 [ -z "$password" ] && {
-    notify-send -u critical "empty string received from rofi"
+    $notify "empty string received from rofi"
     exit 1
 }
 
 pass show -c $password
 
-[ "$?" = "0" ] && notify-send "“$password” copied" \
+[ "$?" = "0" ] && $notify "“$password” copied" \
   "Password copied to clipboard, will be cleared after\
  ${PASSWORD_STORE_CLIP_TIME:-45} seconds" \
  || {
-    notify-send -u critical "error copying password '$password'"
+    $notify "error copying password '$password'"
     exit 1
 }
 
