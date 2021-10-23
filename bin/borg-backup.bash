6df1b41d44c6e870b7058ce57129952ca1d3f74c
İ. Göktuğ Kayaalp <self@gkayaalp.com>
2021-10-23 13:42:09 +0300

    bin/borg-backup.bash: make borg silent

--- bin/borg-backup.bash
+++ bin/borg-backup.bash
@@ -12,7 +12,7 @@ say 'environment looks like:'
 
 env | grep -i borg
 
-borg create --stats --progress --compression lz4 ::{user}-{now} $MYFS/
+borg create --compression lz4 ::{user}-{now} $MYFS/
 
 notify-send -u low 'Borg backup completed' 'Local backup successfully completed'
 
