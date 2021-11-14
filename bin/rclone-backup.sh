e27498c73c3aad4fe943c560f377a07d876b3441
İ. Göktuğ Kayaalp <self@gkayaalp.com>
2021-11-14 14:03:05 +0300

    bin/rclone-backup.sh: add ~/Research to the backup

--- bin/rclone-backup.sh
+++ bin/rclone-backup.sh
@@ -9,7 +9,8 @@ export GK_NOENV=yes
 say starting rclone backup...
 
 rclone copy $HOME/Documents                 encryptifier:/Documents 2>&1
-rclone copy $HOME/Notes                     encryptifier:/Notes 2>&1
+rclone copy $HOME/Research                  encryptifier:/Research  2>&1
+rclone copy $HOME/Notes                     encryptifier:/Notes     2>&1
 
 notify-send -u low 'rclone backup completed' 'Remote backup successfully completed'
 
