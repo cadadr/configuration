502e8e605aad1d5c1241a79845f335de91334059
İ. Göktuğ Kayaalp <self@gkayaalp.com>
2021-12-19 23:32:15 +0300

    ; lib/profile/xdg.sh: fix comments

--- lib/profile/xdg.sh
+++ lib/profile/xdg.sh
@@ -12,6 +12,9 @@ do
 done
 
 # Extend ~/.config.
-
-export XDG_CONFIG_DIRS="$MY/xdg:/etc/xdg" # $XDG_CONFIG_DIRS"
+export XDG_CONFIG_DIRS="$MY/xdg:/etc/xdg"
+# Normally we’d want to include $XDG_CONFIG_DIRS above to include
+# default directories but for some reason it doesn’t work at least on
+# Cinnamon so I’m manually adding /etc/xdg. We *don’t* want ~/.config
+# in here, it’s for XDG_CONFIG_HOME.
 
