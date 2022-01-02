234038c8c36a4c8e45697ed0314212a8a936805d
İ. Göktuğ Kayaalp <self@gkayaalp.com>
2022-01-03 00:09:08 +0300

    systems/common/flatpak.sh: add Steam

--- systems/common/flatpak.sh
+++ systems/common/flatpak.sh
@@ -7,6 +7,6 @@ flatpak --noninteractive install io.bit3.WhatsAppQT
 flatpak --noninteractive install com.github.tchx84.Flatseal
 flatpak --noninteractive install us.zoom.Zoom
 flatpak --noninteractive install com.ozmartians.VidCutter
-
+flatpak --noninteractive install com.valvesoftware.Steam
 
 flatpak override --user --filesystem=$MYFS/dat/lagrange fi.skyjake.Lagrange
