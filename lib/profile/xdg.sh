a6f0daaaaa96e897e6971b01fcf4b8789d9e3428
İ. Göktuğ Kayaalp <self@gkayaalp.com>
2022-01-04 13:57:45 +0300

    lib/profile/xdg.sh: do not set XDG_CONFIG_DIRS

--- lib/profile/xdg.sh
+++ lib/profile/xdg.sh
@@ -10,11 +10,3 @@ do
     mkdir -p "$dir"
     update-desktop-database "$dir"
 done
-
-# Extend ~/.config.
-export XDG_CONFIG_DIRS="$MY/xdg:/etc/xdg"
-# Normally we’d want to include $XDG_CONFIG_DIRS above to include
-# default directories but for some reason it doesn’t work at least on
-# Cinnamon so I’m manually adding /etc/xdg. We *don’t* want ~/.config
-# in here, it’s for XDG_CONFIG_HOME.
-
