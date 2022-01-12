5e11c8711a31d4c1c5f4e6918550e7bbb2797fca
İ. Göktuğ Kayaalp <self@gkayaalp.com>
2022-01-12 16:52:40 +0300

    Add packages.jl

---
+++ packages.jl
@@ -0,0 +1,7 @@
+# packages.jl --- Julia packages
+
+using Pkg
+
+Pkg.add("StatsKit")		# https://juliastats.org/
+Pkg.add("Plots")		# http://docs.juliaplots.org/latest/
+Pkg.add("PlotThemes") 		# themes for JuliaPlots
