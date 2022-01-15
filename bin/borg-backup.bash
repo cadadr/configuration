278d743b33aed9d3bd514ac7824a674122ba665c
İ. Göktuğ Kayaalp <self@gkayaalp.com>
2022-01-15 22:09:32 +0300

    bin/borg-backup.bash: report errors properly

--- bin/borg-backup.bash
+++ bin/borg-backup.bash
@@ -27,6 +27,13 @@ borg create $maybe_progress --compression=lz4 \
      $repo::{user}-{now}_initial-archive      \
      $(bash $MYSYSTEM/full-backup.dirs)
 
-notify-send -u low 'Borg backup completed' 'Local backup successfully completed'
-
-say done finished completed ciao
+exit_code=$?
+if [ "$exit_code" -eq 0 ]; then
+    notify-send -u low \
+                'Borg backup completed' 'Local backup successfully completed'
+    say done finished completed ciao
+else
+    notify-send -u critical \
+                "Borg backup failed with error code ${exit_code}!"
+    say failed with $exit_code
+fi
