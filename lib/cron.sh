58fe69f8d15131d325aabcaed22b52e675f2a2f8
İ. Göktuğ Kayaalp <self@gkayaalp.com>
2022-01-24 23:23:46 +0300

    lib/cron.sh: support i3wm too

--- lib/cron.sh
+++ lib/cron.sh
@@ -1,8 +1,14 @@
 # cron.sh --- common setup for cron scripts
 
-
-env=/proc/$(pgrep -f "cinnamon-session --session cinnamon" -u "$USER" \
-                | cut -d ' ' -f 1)/environ
+# Try to find DBUS session bus address.
+if pgrep i3 >/dev/null 2>&1; then
+    env=/proc/$(pgrep -f "i3 " -u "$USER" | cut -d ' ' -f 1)/environ
+elif pgrep cinnamon-session >/dev/null 2>&1; then
+    env=/proc/$(pgrep -f "cinnamon-session --session cinnamon" -u "$USER" \
+                      | cut -d ' ' -f 1)/environ
+else
+    echo $0: could not locate session process
+fi
 
 if [ -e "$env" ]; then
     export $(cat $env | egrep -z '^DBUS_SESSION_BUS_ADDRESS=' | tr -d '\0')
