957a726b6a3a4946e23177a3c51474361f143bcd
İ. Göktuğ Kayaalp <self@gkayaalp.com>
2022-02-01 18:42:27 +0300

    ; dotfiles/shrc.sh

--- dotfiles/shrc.sh
+++ dotfiles/shrc.sh
@@ -66,6 +66,7 @@ hash -r
 
 ### Kitty:
 
+
 # BEGIN_KITTY_SHELL_INTEGRATION
 if test -n "$KITTY_INSTALLATION_DIR" -a -e "$KITTY_INSTALLATION_DIR/shell-integration/bash/kitty.bash"; then source "$KITTY_INSTALLATION_DIR/shell-integration/bash/kitty.bash"; fi
 # END_KITTY_SHELL_INTEGRATION
