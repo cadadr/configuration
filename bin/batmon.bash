cd9ff96f43a87f556da680fe0e4e0ed987af0c9b
İ. Göktuğ Kayaalp <self@gkayaalp.com>
2022-02-21 08:08:14 +0300

    Add bin/batmon.bash, call it from sappho's crontab
    This script monitors the BAT0 and alerts if the levels are too low.
    Frequency of alerts depends on the crontab.

---
+++ bin/batmon.bash
@@ -0,0 +1,26 @@
+#!/bin/bash
+# batmon.bash --- battery monitor script, for cron
+
+export GK_NOENV=yes
+. $HOME/.profile
+. $MYLIB/cron.sh
+
+path=/sys/class/power_supply
+battery=BAT0
+
+. $path/$battery/uevent
+
+# We use designed power leven instead of actual power level to match
+# i3status.
+x="$POWER_SUPPLY_ENERGY_NOW"
+y="$POWER_SUPPLY_ENERGY_FULL_DESIGN"
+percent_full="$(echo 2k $x $y / 100 \*p | dc | cut -d. -f1)"
+
+if [ "$POWER_SUPPLY_STATUS" = 'Discharging' ] \
+    && [ "$percent_full" -lt 20 ];
+then
+    notify-send -u critical -h int:value:"$percent_full"              \
+	"Low battery" "Battery level critically low ($percent_full%)" \
+	--icon=ac-adapter
+fi
+
