c2c6b4989ba284affbfbe49cef8a18785d17e9df
İ. Göktuğ Kayaalp <self@gkayaalp.com>
2022-04-17 20:16:48 +0300

    bin/rclone-backup.sh: use active system’s specific online backups script

--- bin/rclone-backup.sh
+++ bin/rclone-backup.sh
@@ -8,9 +8,7 @@ export GK_NOENV=yes
 
 say starting rclone backup...
 
-rclone copy $HOME/Documents                 encryptifier:/Documents 2>&1
-rclone copy $HOME/Research                  encryptifier:/Research  2>&1
-rclone copy $HOME/Notes                     encryptifier:/Notes     2>&1
+sh "$MYSYSTEM/online-backup.sh"  2>&1
 
 notify-send -u low 'rclone backup completed' 'Remote backup successfully completed'
 
