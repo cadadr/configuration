77bab15c41520dfe52cedf944ade9982d782e741
İ. Göktuğ Kayaalp <self@gkayaalp.com>
2022-05-06 17:06:47 +0300

    pcmanfm fucking yeeted bloody yeeted
    SEPARATE YOUR DAMN STATE FROM YOUR CONFIG GODDAMIT

--- systems/sappho/desktop-setup.bash
+++ systems/sappho/desktop-setup.bash
@@ -56,7 +56,6 @@ pipewire-pulse           &
 # system tray.
 (sleep 5; flameshot)     &
 nm-applet                &
-# pcmanfm --daemon-mode    &
 volctl                   &
 blueman-applet           &
 picom --config $MY/dotfiles/picom.conf &
