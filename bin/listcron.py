dff0a738de1bfa7d49a248ccb91015cdbf427bc7
İ. Göktuğ Kayaalp <self@gkayaalp.com>
2022-05-22 08:10:25 +0300

    bin/listcron.py: default to short form, add verbose option, some refactoring

--- bin/listcron.py
+++ bin/listcron.py
@@ -4,16 +4,42 @@
 # Adapted from: https://unix.stackexchange.com/questions/181347/time-remaining-for-the-next-run
 
 import subprocess
+import sys
 import re
 
 from croniter import croniter
 from datetime import datetime
 
-lines = subprocess.Popen("crontab -l", shell=True, stdout=subprocess.PIPE).stdout.readlines()
+verbose = False
+
+def usage(exit_code=0):
+    print("listcron.py: usage: listcron.py [-vh]")
+    exit(exit_code)
+
+try:
+    arg = sys.argv[1]
+    if arg == '-v':
+        verbose = True
+    elif arg == '-h':
+        usage()
+    else:
+        usage(1)
+except IndexError:
+    pass
+
+
+lines = subprocess.Popen(
+    "crontab -l", shell=True, stdout=subprocess.PIPE
+).stdout.readlines()
 
 print("Upcoming cron jobs:\n")
-print("min\thr\tday\tmonth\tweek\tdatetime\t\tcommand to be executed")
-print("------------------------------------------------------------------------------------------")
+
+if verbose:
+    print("min\thr\tday\tmonth\tweek\tdatetime\t\tcommand to be executed")
+    print("-" * 90)
+else:
+    print("datetime\t\tcommand to be executed")
+    print("-" * 46)
 
 for line in lines:
     l = line.decode('utf-8').strip()
@@ -21,6 +47,9 @@ for line in lines:
         parts = l.split(maxsplit=5)
         patn = "\t".join(parts[:5])
         i = croniter(patn, datetime.now())
-        print(f"{patn}\t{i.get_next(datetime)}\t{parts[-1]}")
+        patn_ = f"{patn+'	' if verbose else ''}"
+        time  = f"{i.get_next(datetime)}"
+        cmd   = f"{parts[-1]}"
+        print(f"{patn_}{time}\t{cmd}")
 
 
