a2441c4b8d06db3fe2aed77a7a32b5e67836a6d3
İ. Göktuğ Kayaalp <self@gkayaalp.com>
2022-08-26 04:06:38 +0300

    systems/sappho/dock.bash (on_docked): use both screens

--- systems/sappho/dock.bash
+++ systems/sappho/dock.bash
@@ -30,7 +30,7 @@ on_docked(){
     xinput set-prop "Logitech USB Trackball" 'libinput Scroll Method Enabled' {0,0,1}
 
     # Monitors
-    /bin/sh /home/cadadr/.screenlayout/sappho-docked-only-external.sh
+    /bin/sh /home/cadadr/.screenlayout/sappho-docked-both.sh
 }
 
 on_undocked(){
