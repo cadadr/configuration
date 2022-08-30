34a206e4c11cd3ab6dc12d5c55d381025e143b0b
İ. Göktuğ Kayaalp <self@gkayaalp.com>
2022-08-31 00:45:41 +0300

    Move kitty terminal integration snippet from generic shrc to rc.bash

--- lib/rc.bash
+++ lib/rc.bash
@@ -36,3 +36,9 @@ if [ "$TERM" = "dumb" ]; then
 fi
 
 export HISTTIMEFORMAT='%s'
+
+### Kitty:
+
+if test -n "$KITTY_INSTALLATION_DIR" -a -e "$KITTY_INSTALLATION_DIR/shell-integration/bash/kitty.bash"; then
+    source "$KITTY_INSTALLATION_DIR/shell-integration/bash/kitty.bash"
+fi
