02b80a33965c5fbfedf9c8cc30c85a387f6de10c
İ. Göktuğ Kayaalp <self@gkayaalp.com>
2022-09-10 06:51:33 +0300

    Add bin/i3-move-to-workspace.bash

---
+++ bin/i3-move-to-workspace.bash
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# i3-move-to-workspace.bash --- fuzzy-match i3wm workspaces and move active container
+
+# Adapted from https://cyberchaos.dev/kookie/nomicon/-/blob/main/\
+#   infra/libkookie/modules/workstation/ui/i3/core/tools/i3-switch.nix
+
+# bash strict mode
+set -euo pipefail
+IFS=$'\n\t'
+
+workspace="$(i3-msg -t get_workspaces \
+                    | jq -r -M '.[] | .name' | sort -u \
+                    | rofi -dmenu)"
+
+i3-msg move container to workspace $workspace
