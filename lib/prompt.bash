3c12c52580e41f01e9b89a153e7643407b9b5188
İ. Göktuğ Kayaalp <self@gkayaalp.com>
2022-11-27 16:29:33 +0300

    lib/prompt.bash: fix prompt clobbered, add simple prompt
    Turns out what clobbered the prompt line was bp_lastcmdexit,
    specifically, its output being on the same line as the cursor. I suppose
    multiple ``tput sgr0'' on the same line break something. With this
    commit, the error message gets its own line.
    
    Furthermore, a simple prompt is added, all the function local variables
    are marked as such, and also the reprinting of the erroneous exit message
    is suppressed by bp_lastcmdexit tracking and checking on HISTCMD.

--- lib/prompt.bash
+++ lib/prompt.bash
@@ -1,14 +1,37 @@
 # prompt.bash --- bash prompt
 
+_bp_histcmd="$HISTCMD"
+
+# Parameter to set the prompt style on the fly.
+BP_PROMPT_STYLE=simple
+
 bp_lastcmdexit () {
-    xit=$?
-    [ $xit -ne 0 ] && printf \
-	"$(tput setab 9)$(tput setaf 16) $xit!$(tput sgr0) "
+    local last_cmd_exit_code=$?
+    # Check if history index advanced, and only print anything if that
+    # happened. When user hits Return in an empty prompt, or when a command
+    # line is cancelled with ^C, the history index is not updated, it stays
+    # the same number as before the submission/cancellation of that command
+    # line. We exploit this to avoid printing the error message for the same
+    # command multiple times should some empty prompts be submitted, or
+    # a command line be cancelled.
+    #
+    # XXX: does not work when the submitted command is a comment: the value
+    # of \# in the prompt stays the same, but apparently HISTCMD is
+    # incremented.
+    if [ "$_bp_histcmd" -ne "$HISTCMD" ]; then
+	local msg="command line failed with exit code"
+	# XXX(2022-11-27): this clobbers the prompt if it's on the same line with it,
+	# so i break the line after it and made it a bit longer just to make it
+	# look nicer as such.
+	[ $last_cmd_exit_code -ne 0 ] && \
+	    printf "$(tput setab 9)$(tput setaf 16)$msg: $last_cmd_exit_code$(tput sgr0) \n"
+    fi
+    _bp_histcmd="$HISTCMD"
 }
 
 bp_queue () {
     if [ -d "$MAILQUEUE" ]; then
-        count="$(( $(ls $MAILQUEUE | wc -l) / 2 ))"
+        local count="$(( $(ls $MAILQUEUE | wc -l) / 2 ))"
         [ "0" != "$count" ] && echo " <✉:$count> ";
     fi
     true
@@ -49,10 +72,10 @@ bp_is_repo () (
 )
 
 bp_branch () {
-    maybe_repo=$(bp_is_repo)
+    local maybe_repo=$(bp_is_repo)
     if [ -n "$maybe_repo" ]; then
-	branch=$(bp_${maybe_repo}_branch)
-	dirty=''
+	local branch=$(bp_${maybe_repo}_branch)
+	local dirty=''
 	if bp_${maybe_repo}_dirty; then
 	    dirty="#"
 	fi
@@ -66,8 +89,8 @@ bp_venv () {
     # If a python virtual environment is active, provide relevant
     # info.
     if [ -n "$VIRTUAL_ENV" ]; then
-	venvname="$(realpath --relative-to=$PWD $VIRTUAL_ENV)"
-	venvpyver="v$($venvname/bin/python --version | cut -d ' ' -f 2)"
+	local venvname="$(realpath --relative-to=$PWD $VIRTUAL_ENV)"
+	local venvpyver="v$($venvname/bin/python --version | cut -d ' ' -f 2)"
 	echo " <venv:$venvname@$venvpyver> "
     fi
 }
@@ -84,31 +107,31 @@ bp_termwidth () {
 }
 
 bp_prompt_long_p () {
-    ps1_expanded="${1@P}"
-    ps1_expanded="$(echo -ne $ps1_expanded | sed $'s/\e\\[[0-9;:]*[a-zA-Z]//g')"
+    local ps1_expanded="${1@P}"
+    local ps1_expanded="$(echo -ne $ps1_expanded | sed $'s/\e\\[[0-9;:]*[a-zA-Z]//g')"
     [ ${#ps1_expanded} -gt $(( $(bp_termwidth) / 2 )) ]
     return $?
 }
 
-bp_procmd () {
-    # Should always be the first thing in this function so that it
-    # is run just after the last evaluated command whose exit code
-    # is in $?.
-    bp_lastcmdexit
-
-    bold="\[$(tput bold)\]"
-    reset="\[$(tput sgr0)\]"
-
-    # if xterm, colour
+bp_colour_or_bold () {
     case $TERM in
+	# if xterm, colour
 	xterm*|alacritty)
-	    color="$(seq 1 16 | grep -ve '[789]' -e '1' -e '16' | shuf -n 1)"
-	    bold="\[$(tput setaf $color)\]"
+	    local color="$(seq 1 16 | grep -ve '[789]' -e '1' -e '16' | shuf -n 1)"
+	    printf "\[$(tput setaf $color)\]"
 	    ;;
 
-	* ) # Do nothing.
+	# Otherwise, just bold face
+	* ) printf "\[$(tput bold)\]"
 	    ;;
     esac
+}
+
+# Info dense prompt that automatically breaks up into two lines if
+# it's too wide.
+bp_adaptive_prompt () {
+    local bold="$(bp_colour_or_bold)"
+    local reset="\[$(tput sgr0)\]"
 
     PS1="$bold"'#'"\#$reset \D{%F %H:%M} "
     PS1+="$(bp_venv)$(bp_guix)$(bp_queue)$bold\u@\H$reset:\w"
@@ -119,6 +142,21 @@ bp_procmd () {
     fi
 
     PS1+="$bold(\jj/^$SHLVL)\$$reset "
+}
 
-    unset bold reset ps1_expanded
+# Simplistic prompt with history index and job control info & shlvl
+bp_simple_prompt () {
+    local bold="$(bp_colour_or_bold)"
+    local reset="\[$(tput sgr0)\]"
+
+    PS1="$bold[\#: \jj/^$SHLVL] \$$reset "
+}
+
+bp_procmd () {
+    # Should always be the first thing in this function so that it
+    # is run just after the last evaluated command whose exit code
+    # is in $?.
+    bp_lastcmdexit
+
+    bp_${BP_PROMPT_STYLE}_prompt
 }
