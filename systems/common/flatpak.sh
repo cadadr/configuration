b7979139ad191ce323d89987c04770229eafa76a
İ. Göktuğ Kayaalp <self@gkayaalp.com>
2023-01-30 11:41:06 +0300

    systems/common/flatpak.sh: remove useless flatpaks

--- systems/common/flatpak.sh
+++ systems/common/flatpak.sh
@@ -2,8 +2,6 @@
 
 flatpak --noninteractive install fi.skyjake.Lagrange
 flatpak --noninteractive install com.spotify.Client
-flatpak --noninteractive install org.telegram
-flatpak --noninteractive install io.bit3.WhatsAppQT
 flatpak --noninteractive install com.github.tchx84.Flatseal
 flatpak --noninteractive install us.zoom.Zoom
 flatpak --noninteractive install com.ozmartians.VidCutter
