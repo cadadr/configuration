af8cfb3c7e4c38947c33f84a15958ed3caf93ed3
İ. Göktuğ Kayaalp <self@gkayaalp.com>
2023-02-01 03:28:14 +0300

    ; systems/sappho/stage02.bash: line break

--- systems/sappho/stage02.bash
+++ systems/sappho/stage02.bash
@@ -149,7 +149,8 @@ echo
 usermod -aG kvm $username
 
 # Add flatpak remote
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+flatpak remote-add --if-not-exists flathub \
+        https://flathub.org/repo/flathub.flatpakrepo
 
 ### Enable services:
 
