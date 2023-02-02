1f4860d0aa94eb0ae2f7fbb672503f3563eb563d
İ. Göktuğ Kayaalp <self@gkayaalp.com>
2023-02-02 07:09:56 +0300

    systems/sappho/desktop-setup.bash: fix sectioning

--- systems/sappho/desktop-setup.bash
+++ systems/sappho/desktop-setup.bash
@@ -150,10 +150,8 @@ export XCURSOR_SIZE=$mouse_size
 ### Launch window manager:
 (sleep 10; notify-send -t 2000 welcome "welcome to $(hostname)!" ) &
 
-### Detect if docked:
-
-# Because udev is a pile of crap, I need to do this manually... d'uh..
-
+# Detect if docked. Because udev is a pile of crap, I need to do this
+# manually... d'uh..
 (cat /sys/devices/platform/dock.*/docked | grep 1) >/dev/null \
     && $MYSYSTEM/dock.bash docked
 
