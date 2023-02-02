0f413cd5e906597c464604472ba288b0173c3869
İ. Göktuğ Kayaalp <self@gkayaalp.com>
2023-02-02 07:09:39 +0300

    lib/cron.sh: use ~/.dbus to find dbus info if possible

--- lib/cron.sh
+++ lib/cron.sh
@@ -1,16 +1,23 @@
 # cron.sh --- common setup for cron scripts
 
-# Try to find DBUS session bus address.
-if pgrep i3 >/dev/null 2>&1; then
-    env=/proc/$(pgrep -f "i3 " -u "$USER" | cut -d ' ' -f 1)/environ
-elif pgrep cinnamon-session >/dev/null 2>&1; then
-    env=/proc/$(pgrep -f "cinnamon-session --session cinnamon" -u "$USER" \
-                      | cut -d ' ' -f 1)/environ
+# Figure out DBus info.
+if [ -e /var/lib/dbus/machine-id ] \
+    && [ -e "$HOME/.dbus/session-bus/$(cat /var/lib/dbus/machine-id)-0" ];
+then
+    . $HOME/.dbus/session-bus/$(cat /var/lib/dbus/machine-id)-0
+    export DBUS_SESSION_BUS_ADDRESS DBUS_SESSION_BUS_PID
+    export DBUS_SESSION_BUS_WINDOWID
 else
-    echo $0: could not locate session process
-fi
+    if pgrep i3 >/dev/null 2>&1; then
+        env=/proc/$(pgrep -f "i3 " -u "$USER" | cut -d ' ' -f 1)/environ
+    elif pgrep cinnamon-session >/dev/null 2>&1; then
+        env=/proc/$(pgrep -f "cinnamon-session --session cinnamon" -u "$USER" \
+                          | cut -d ' ' -f 1)/environ
+    else
+        echo $0: could not locate session process
+    fi
 
-if [ -e "$env" ]; then
-    export $(cat $env | egrep -z '^DBUS_SESSION_BUS_ADDRESS=' | tr -d '\0')
+    if [ -e "$env" ]; then
+        export $(cat $env | egrep -z '^DBUS_SESSION_BUS_ADDRESS=' | tr -d '\0')
+    fi
 fi
-
