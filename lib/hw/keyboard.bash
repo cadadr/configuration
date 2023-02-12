cadadr/configuration

commit 827e5856c83fbe92bad2842a2ab5e912aca1b065

systems/sappho/dock.bash (on_docked): improve trackball mappings

systems/sappho/dock.bash
@@ -27,11 +27,15 @@ on_docked(){
     setxkbmap gb
     setxkbmap -option  ctrl:nocaps
 
-    # Trackball [if present]
-    # Hold the smaller left button to scroll in all directions.
-    xinput set-prop "Logitech USB Trackball" 'libinput Scroll Method Enabled' {0,0,1} || true
-    # Instead of holding, the button 8 toggles scroll mode.
-    xinput set-prop "Logitech USB Trackball" "libinput Button Scrolling Button Lock Enabled" 1 || true
+    # Logitech USB Trackball
+    #
+    # Set small button on the right as middle click, and disable history
+    # navigation.
+    xinput set-button-map "Logitech USB Trackball" \
+        1 0 3 4 5 6 7 2 0
+    # Make button 8 (left small) toggle scroll mode.
+    xinput set-prop "Logitech USB Trackball" \
+        "libinput Button Scrolling Button Lock Enabled" 1 || true
 
     # Monitors
     /bin/sh $HOME/.screenlayout/sappho-docked-only-external-vertical.sh

commit 5028e5e4fea2f437ab0ea3dec1014ab6620df9dd

Extract common hardware settings to scripts under lib/hw
Also fixes a bug introduced in
827e5856c83fbe92bad2842a2ab5e912aca1b065 that leads to trackball
scrolling when docked not working.

dotfiles/xsessionrc
@@ -21,8 +21,7 @@ dconf load / < $MYSYSTEM/dconf.dump
 
 ### Input:
 #### Keyboard:
-setxkbmap gb
-setxkbmap -option "ctrl:nocaps"
+bash $MYLIB/hw/keyboard.bash
 
 ### System specific desktop setup:
 
@@ -52,16 +51,24 @@ if [ -e "$MYSYSTEM/desktop-setup.bash" ]; then
                     bash $MYSYSTEM/desktop-setup.bash
             fi
             ;;
+
         # GDM etc. sets it like this, which should do the dbus
         # housekeeping itself.
-        x11) bash $MYSYSTEM/desktop-setup.bash ;;
+        x11)
+            bash $MYSYSTEM/desktop-setup.bash
+            ;;
+
         # A wayland session should not run this script but in case
         # that happens by accident it’s nice to have this in the logs
         # somehow.
         wayland)
-            echo "$0: *** WAYLAND SESSION SETUP NOT IMPLEMENTED YET ***" ;;
+            echo "$0: *** WAYLAND SESSION SETUP NOT IMPLEMENTED YET ***"
+            ;;
+
         # This case should be unreachable but in case it is, it’s nice
         # to have a line in the logs.
-        *) echo "$0: Unknown session type: XDG_SESSION_TYPE=$XDG_SESSION_TYPE (this shouldn't happen)" ;;
+        *)
+            echo "$0: Unknown session type: XDG_SESSION_TYPE=$XDG_SESSION_TYPE (this shouldn't happen)"
+            ;;
     esac
 fi

lib/hw/keyboard.bash
@@ -1,0 +1,9 @@
+#!/usr/bin/env bash
+# keyboard.bash --- default keyboard settings
+
+# bash strict mode
+set -euo pipefail
+IFS=$'\n\t'
+
+setxkbmap gb
+setxkbmap -option  ctrl:nocaps

lib/hw/logitech_trackman.bash
@@ -1,0 +1,22 @@
+#!/usr/bin/env bash
+# logitech_trackman.bash --- logitech trackman USB trackball settings
+
+# bash strict mode
+set -euo pipefail
+IFS=$'\n\t'
+
+
+# Set small button on the right as middle click, and disable history
+# navigation.
+xinput set-button-map "Logitech USB Trackball" \
+       1 0 3 4 5 6 7 2 0
+
+# Enable scrolling. By default, the left small button needs to be
+# pressed and held to use the track ball to scroll. But that’s an
+# ergonomic nightmare so ...
+xinput set-prop "Logitech USB Trackball" 'libinput Scroll Method Enabled' \
+       {0,0,1}
+
+# ... make button 8 (left small) toggle scroll mode.
+xinput set-prop "Logitech USB Trackball" \
+       "libinput Button Scrolling Button Lock Enabled" 1

systems/sappho/dock.bash
@@ -8,6 +8,8 @@ if [ "$2" != "forked" ]; then
     exit
 fi
 
+. $HOME/.profile
+
 # bash strict mode
 set -euo pipefail
 
@@ -23,19 +25,12 @@ on_docked(){
     # Wait for all devices to be detected.
     sleep 5
 
-    # Keyboard
-    setxkbmap gb
-    setxkbmap -option  ctrl:nocaps
+    # Make sure keyboard settings apply to any external keyboard too.
+    bash $MYLIB/hw/keyboard.bash
 
-    # Logitech USB Trackball
-    #
-    # Set small button on the right as middle click, and disable history
-    # navigation.
-    xinput set-button-map "Logitech USB Trackball" \
-        1 0 3 4 5 6 7 2 0
-    # Make button 8 (left small) toggle scroll mode.
-    xinput set-prop "Logitech USB Trackball" \
-        "libinput Button Scrolling Button Lock Enabled" 1 || true
+    # If the USB trackball is attached, configure it.
+    xinput list | grep "Logitech USB Trackball" >/dev/null 2>&1 \
+        && bash $MYLIB/hw/logitech_trackman.bash
 
     # Monitors
     /bin/sh $HOME/.screenlayout/sappho-docked-only-external-vertical.sh