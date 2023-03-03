8fe00424b25b778d9b6bfe978f81fd7063a12e4a
İ. Göktuğ Kayaalp <self@gkayaalp.com>
2023-03-03 18:44:18 +0300

    systems/sappho/stage01.bash: add xmirror

--- systems/sappho/stage01.bash
+++ systems/sappho/stage01.bash
@@ -95,7 +95,7 @@ echo
 
 xbps-install -Sy -R https://alpha.de.repo.voidlinux.org/current \
              -r /mnt base-system cryptsetup grub-x86_64-efi lvm2 \
-                     NetworkManager git borg wget w3m
+                     NetworkManager git borg wget w3m xmirror
 
 echo
 echo === Configure new system...
