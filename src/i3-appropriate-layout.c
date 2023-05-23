32e0619dfc2de929d1197dc0f833b50f8bc04731
İ. Göktuğ Kayaalp <self@gkayaalp.com>
2023-05-23 09:51:52 +0300

    src/i3-appropriate-layout.c: reindent

--- src/i3-appropriate-layout.c
+++ src/i3-appropriate-layout.c
@@ -10,88 +10,82 @@
 #include <xcb/randr.h>
 
 enum orientation
-  {
+{
     Vertical,
     Horizontal
-  };
+};
 
 void
 i3command(const gchar *command, i3ipcCon *ws)
 {
-  GError *err = NULL;
+    GError *err = NULL;
 
-  g_printf("running command: %s for workspace %s...\n", command, i3ipc_con_get_name(ws));
-  i3ipc_con_command(ws, command, &err);
+    g_printf("running command: %s for workspace %s...\n", command, i3ipc_con_get_name(ws));
+    i3ipc_con_command(ws, command, &err);
 
-  if(err)
-    {
-      g_printf("i3 IPC command error: %s\n", err->message);
-      exit(EXIT_FAILURE);
+    if(err) {
+        g_printf("i3 IPC command error: %s\n", err->message);
+        exit(EXIT_FAILURE);
     }
 }
 
 void
 maybe_set_layout(gpointer data, gpointer user_data)
 {
-  i3ipcCon *ws = data;
-  enum orientation orientation = *(enum orientation *) user_data;
-  const gchar *layout, *name;
+    i3ipcCon *ws = data;
+    enum orientation orientation = *(enum orientation *) user_data;
+    const gchar *layout, *name;
 
-  g_object_get(ws, "layout", &layout, NULL);
-  name = i3ipc_con_get_name(ws);
+    g_object_get(ws, "layout", &layout, NULL);
+    name = i3ipc_con_get_name(ws);
 
-  g_printf("layout for ws %s is %s.\n", name, layout);
-  g_printf("screen layout is %s\n", orientation == Horizontal ? "horizontal" : "vertical");
+    g_printf("layout for ws %s is %s.\n", name, layout);
+    g_printf("screen layout is %s\n", orientation == Horizontal ? "horizontal" : "vertical");
 
-  if (strstr(layout, "split") != NULL)
-    {
-      if (orientation == Horizontal)
-        i3command("layout splith", ws);
-      else if (orientation == Vertical)
-        i3command("layout splitv", ws);
-      else
-        g_printf("nothing to be done for ws %s\n", name);
+    if (strstr(layout, "split") != NULL) {
+        if (orientation == Horizontal)
+            i3command("layout splith", ws);
+        else if (orientation == Vertical)
+            i3command("layout splitv", ws);
+        else
+            g_printf("nothing to be done for ws %s\n", name);
     }
 }
 
 enum orientation
 get_display_orientation(void)
 {
-  xcb_connection_t *connection;
-  const xcb_setup_t *setup;
-  xcb_screen_iterator_t iter;
-  xcb_screen_t *screen;
-  xcb_window_t root;
-  xcb_randr_get_screen_info_cookie_t cookie;
-  xcb_generic_error_t *error;
-  xcb_randr_get_screen_info_reply_t *reply;
-  uint16_t rotation;
-  connection = xcb_connect(NULL, NULL);
-  setup = xcb_get_setup(connection);
+    xcb_connection_t *connection;
+    const xcb_setup_t *setup;
+    xcb_screen_iterator_t iter;
+    xcb_screen_t *screen;
+    xcb_window_t root;
+    xcb_randr_get_screen_info_cookie_t cookie;
+    xcb_generic_error_t *error;
+    xcb_randr_get_screen_info_reply_t *reply;
+    uint16_t rotation;
+    connection = xcb_connect(NULL, NULL);
+    setup = xcb_get_setup(connection);
 
-  // Get first screen.
-  iter = xcb_setup_roots_iterator(setup);
-  screen = iter.data;
-  root = screen->root;
+    // Get first screen.
+    iter = xcb_setup_roots_iterator(setup);
+    screen = iter.data;
+    root = screen->root;
 
-  cookie = xcb_randr_get_screen_info(connection, root);
-  reply = xcb_randr_get_screen_info_reply(connection, cookie, &error);
+    cookie = xcb_randr_get_screen_info(connection, root);
+    reply = xcb_randr_get_screen_info_reply(connection, cookie, &error);
 
-  if (error)
-    {
-      printf("xcb error: code %d\n", error->error_code);
-      exit(EXIT_FAILURE);
-    }
-  else
-    {
-      rotation = reply->rotation;
-      switch (rotation)
-        {
+    if (error) {
+        printf("xcb error: code %d\n", error->error_code);
+        exit(EXIT_FAILURE);
+    } else {
+        rotation = reply->rotation;
+        switch (rotation) {
         case 010:
         case 01000:
-          return Vertical;
+            return Vertical;
         default:
-          return Horizontal;
+            return Horizontal;
         }
     }
 }
@@ -99,17 +93,17 @@ get_display_orientation(void)
 gint
 main(const int argc, const char **argv)
 {
-  i3ipcConnection *connection;
-  i3ipcCon *tree;
-  GList *workspaces;
-  enum orientation orientation;
+    i3ipcConnection *connection;
+    i3ipcCon *tree;
+    GList *workspaces;
+    enum orientation orientation;
 
-  orientation = get_display_orientation();
-  connection = i3ipc_connection_new(NULL, NULL);
-  tree = i3ipc_connection_get_tree(connection, NULL);
-  workspaces = i3ipc_con_workspaces(tree);
+    orientation = get_display_orientation();
+    connection = i3ipc_connection_new(NULL, NULL);
+    tree = i3ipc_connection_get_tree(connection, NULL);
+    workspaces = i3ipc_con_workspaces(tree);
 
-  g_list_foreach(workspaces, maybe_set_layout, &orientation);
+    g_list_foreach(workspaces, maybe_set_layout, &orientation);
 
-  exit(EXIT_SUCCESS);
+    exit(EXIT_SUCCESS);
 }
