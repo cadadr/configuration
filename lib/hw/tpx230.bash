240ceb8bdd3f3669d2c9d9ede3bf1cd0e28b2844
İ. Göktuğ Kayaalp <self@gkayaalp.com>
2023-05-31 14:12:01 +0300

    Add lib/hw/tpx230.bash
    Settings for ThinkPad x230, extracted from
    systems/sappho/desktop-setup.bash.

---
+++ lib/hw/tpx230.bash
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# tpx230.bash --- hardware settings for ThinkPad x230
+
+# bash strict mode
+set -euo pipefail
+IFS=$'\n\t'
+
+
+# Disable the annoying ThinkPad touchpad
+xinput set-prop 'SynPS/2 Synaptics TouchPad' 'Device Enabled' 0
+
+# Natural scrolling with the nipple
+xinput set-prop 'TPPS/2 IBM TrackPoint' 'libinput Natural Scrolling Enabled' 1
