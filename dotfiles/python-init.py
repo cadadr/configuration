7a96275c3a4f84f7177d7ae6991478193c340699
İ. Göktuğ Kayaalp <self@gkayaalp.com>
2023-06-22 00:45:16 +0300

    dotfiles/python-init.py: define ‘dumpsesh’ to save history to a given file

--- dotfiles/python-init.py
+++ dotfiles/python-init.py
@@ -1,5 +1,12 @@
 import sys, os
 
+
 # This will print tuples in python 2, but who cares about python 2?
 print("Interpreter at:", sys.executable)
 print("Working directory:", os.getcwd())
+
+
+def dumpsesh(outfile):
+    "Dump session history to OUTFILE."
+    import readline
+    readline.write_history_file(outfile)
